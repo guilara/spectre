c0147a6002d8f15a9328f6db5a79377be394e3bb
Guillermo Lara <guillermo.lara@black-holes.org>
2023-01-10 16:36:51 +0000

    add pybind stl lib to python binding

--- src/NumericalAlgorithms/LinearOperators/Python/PowerMonitors.cpp
+++ src/NumericalAlgorithms/LinearOperators/Python/PowerMonitors.cpp
@@ -4,6 +4,7 @@
 #include "NumericalAlgorithms/LinearOperators/Python/PowerMonitors.hpp"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "DataStructures/DataVector.hpp"
 #include "NumericalAlgorithms/LinearOperators/PowerMonitors.hpp"
@@ -27,21 +28,21 @@ void bind_power_monitor_array(py::module& m) {
         return PowerMonitors::power_monitor_array(input_data_vector, mesh);
       },
       py::arg("DataVector"), py::arg("mesh"),
-      py::return_value_policy::reference, "PowerMonitors array ");
+      py::return_value_policy::copy, "PowerMonitors array ");
   m.def(
       "power_monitor_array",
       [](const DataVector& input_data_vector, const Mesh<2>& mesh) {
         return PowerMonitors::power_monitor_array(input_data_vector, mesh);
       },
       py::arg("DataVector"), py::arg("mesh"),
-      py::return_value_policy::reference, "PowerMonitors array ");
+      py::return_value_policy::copy, "PowerMonitors array ");
   m.def(
       "power_monitor_array",
       [](const DataVector& input_data_vector, const Mesh<3>& mesh) {
         return PowerMonitors::power_monitor_array(input_data_vector, mesh);
       },
       py::arg("DataVector"), py::arg("mesh"),
-      py::return_value_policy::reference, "PowerMonitors array ");
+      py::return_value_policy::copy, "PowerMonitors array ");
 }
 
 }  // namespace PowerMonitors::py_bindings
