8fbd93a694b81bf0a889010cd13c1602cec6eb61
Guillermo Lara <guillermo.lara@black-holes.org>
2023-01-30 14:45:42 +0000

    Add python bindings

--- src/NumericalAlgorithms/LinearOperators/Python/PowerMonitors.cpp
+++ src/NumericalAlgorithms/LinearOperators/Python/PowerMonitors.cpp
@@ -22,12 +22,35 @@ void bind_power_monitors_impl(py::module& m) {  // NOLINT
           &power_monitors<Dim>),
         py::arg("input_data_vector"), py::arg("mesh"));
 }
+
+template <size_t Dim>
+void bind_relative_truncation_error_impl(py::module& m) {  // NOLINT
+  m.def("relative_truncation_error",
+        py::overload_cast<const DataVector&, const Mesh<Dim>&>(
+            &relative_truncation_error<Dim>),
+        py::arg("input_data_vector"), py::arg("mesh"));
+}
+
+template <size_t Dim>
+void bind_error_estimate_impl(py::module& m) {  // NOLINT
+  m.def("error_estimate",
+        py::overload_cast<const DataVector&, const Mesh<Dim>&>(
+            &error_estimate<Dim>),
+        py::arg("input_data_vector"), py::arg("mesh"));
+}
+
 }  // namespace
 
 void bind_power_monitors(py::module& m) {
   bind_power_monitors_impl<1>(m);
   bind_power_monitors_impl<2>(m);
   bind_power_monitors_impl<3>(m);
+  bind_relative_truncation_error_impl<1>(m);
+  bind_relative_truncation_error_impl<2>(m);
+  bind_relative_truncation_error_impl<3>(m);
+  bind_error_estimate_impl<1>(m);
+  bind_error_estimate_impl<2>(m);
+  bind_error_estimate_impl<3>(m);
 }
 
 }  // namespace PowerMonitors::py_bindings
