1a35ad4ec67a0c7f2804d067d7bf3753beab607e
Guillermo Lara <guillermo.lara@black-holes.org>
2023-02-11 11:27:56 +0000

    Add option tags
    Add mass option tags

--- src/Evolution/Systems/CurvedScalarWave/Sources/Tags.hpp
+++ src/Evolution/Systems/CurvedScalarWave/Sources/Tags.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "DataStructures/DataBox/Tag.hpp"
+#include "Options/Options.hpp"
 
 namespace CurvedScalarWave::Sources {
 
@@ -13,6 +14,33 @@ struct ScalarSource : db::SimpleTag {
   using type = Scalar<DataVector>;
 };
 
-}  // namespace Tags
+} // namespace Tags
+
+namespace OptionTags {
+
+/*!
+ * \brief The constraint damping parameter for divergence cleaning of electric
+ * fields.
+ */
+struct ScalarMass {
+  static std::string name() { return "ScalarMass"; }
+  using type = double;
+  static constexpr Options::String help{
+      "Mass of the scalar field in code units"};
+  // using group = XXX;
+};
+
+}  // namespace OptionTags
+
+namespace Tags {
+
+struct ScalarMass : db::SimpleTag {
+  using type = double;
+  using option_tags = tmpl::list<OptionTags::ScalarMass>;
+  static constexpr bool pass_metavariables = false;
+  static double create_from_options(const double mass_psi) { return mass_psi; }
+};
+
+} // namespace Tags
 
 }  // namespace CurvedScalarWave::Sources
