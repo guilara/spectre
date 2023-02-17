500de971e57b671f029bd5b8ab718c0474210f7f
Guillermo Lara <guillermo.lara@black-holes.org>
2023-02-17 19:42:40 +0000

    Add stuff to test

--- tests/Unit/PointwiseFunctions/GeneralRelativity/Test_SpacetimeRiemann.cpp
+++ tests/Unit/PointwiseFunctions/GeneralRelativity/Test_SpacetimeRiemann.cpp
@@ -53,14 +53,14 @@ void test_background__spacetime() {
       wrapped_ks_solution{mass, spin, center};
   const gr::Solutions::KerrSchild& ks_solution{mass, spin, center};
 
-  const DataVector data_vector(10, 1.0);
-  Parallel::printf("KerrSchild::volume_dim = %d \n",
-                   gr::Solutions::KerrSchild::volume_dim);
+  // Coordinate stuff
+  const DataVector data_vector(9, 1.0);
   const tnsr::I<DataVector, gr::Solutions::KerrSchild::volume_dim,
                 Frame::Inertial>
       x{data_vector};
   const double t = 1.0;
 
+  // Extract GH variables (and more) from WrappedGr
   const auto wrapped_gh_vars = wrapped_ks_solution.variables(
       x, t,
       tmpl::list<
@@ -71,6 +71,7 @@ void test_background__spacetime() {
           GeneralizedHarmonic::Tags::Phi<gr::Solutions::KerrSchild::volume_dim,
                                          Frame::Inertial>>{});
 
+  // Extract Kerr-Shild variables
   const auto ks_vars = ks_solution.variables(
       x, t,
       tmpl::list<gr::Tags::SpatialChristoffelFirstKind<3, ::Frame::Inertial,
@@ -94,47 +95,26 @@ void test_background__spacetime() {
   const auto& christ_down_trace =
       get<gr::Tags::TraceSpatialChristoffelSecondKind<3, ::Frame::Inertial,
                                                       DataVector>>(ks_vars);
-  // const DataVector christ_data = christ_down_trace.get();
 
-  Parallel::printf("Size of spacetime_metric: %d \n", spacetime_metric.size());
-  Parallel::printf("Size of pi : %d \n", pi.size());
-  Parallel::printf("Size of phi : %d \n", phi.size());
-  Parallel::printf("Size of christ_down_trace : %d \n",
-                   christ_down_trace.size());
-  // for (size_t index = 0; index < spacetime_metric.size(); ++index) {
-  //   Parallel::printf("index: %d \n", index);
-  //   Parallel::printf("spacetime_metric[index]: %lf \n",
-  //                    spacetime_metric[index]);
-  //   Parallel::printf("pi[index]: %lf \n", pi[index]);
-  // }
-  // for (size_t index = 0; index < christ_down_trace.size(); ++index) {
-  //   Parallel::printf("christ_down_trace[index]: %lf \n",
-  //                    christ_down_trace[index]);
-  // }
 
-  // const DataVector check_vec{0.0, 0.0, 0.0};
-  const auto check_tensor =
-      make_with_value<tnsr::I<DataVector, 3>>(christ_down_trace.size(), 0.0);
-  const auto check_tensor_2 =
-      make_with_value<tnsr::aa<DataVector, 3, ::Frame::Inertial>>(
-          data_vector.size(), 0.0);
-  // Parallel::printf("check size: %d \n", check_vec.size());
 
-  std::ostringstream os;
+  const auto& deriv_test =
+      partial_derivative(spacetime_metric, mesh, inverse_jacobian);
 
-  os << get_output(check_tensor_2);
+  //   const auto check_tensor =
+  //       make_with_value<tnsr::I<DataVector, 3>>(christ_down_trace.size(),
+  //       0.0);
+  //   const auto check_tensor_2 =
+  //       make_with_value<tnsr::aa<DataVector, 3, ::Frame::Inertial>>(
+  //           data_vector.size(), 0.0);
 
-  Parallel::printf(os.str());
-
-  for (size_t index = 0; index < 3; ++index) {
-  }
+  //   std::ostringstream os;
+  //   os << get_output(check_tensor_2);
+  //   Parallel::printf(os.str());
 
   // CHECK_ITERABLE_APPROX(christ_down_trace, check_tensor);
   // CHECK_ITERABLE_APPROX(spacetime_metric, check_tensor_2);
 
-  //   auto test_riemann_tensor =
-  //       spacetime_riemann_tensor<DataVector, 4_st, ::Frame::Inertial>(
-  //           pi, phi, deriv_pi, deriv_phi, inverse_spatial_metric);
 }
 
 SPECTRE_TEST_CASE("Unit.PointwiseFunctions.GeneralRelativity.SpacetimeRiemann.",
