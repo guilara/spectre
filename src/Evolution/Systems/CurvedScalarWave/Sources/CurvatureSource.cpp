0ad72f2007efbb3f09d66de58343f54ad1305e28
Guillermo Lara <guillermo.lara@black-holes.org>
2023-02-20 08:42:23 +0000

    Rewrite coupling function and add mass

--- src/Evolution/Systems/CurvedScalarWave/Sources/CurvatureSource.cpp
+++ src/Evolution/Systems/CurvedScalarWave/Sources/CurvatureSource.cpp
@@ -18,20 +18,29 @@ Scalar<DataVector> coupling_function_prime(const Scalar<DataVector>& psi,
   return result;
 }
 
+void multiply_by_coupling_function_prime(
+    gsl::not_null<Scalar<DataVector>*> scalar_source,
+    const Scalar<DataVector>& psi, const double first_coupling_psi,
+    const double second_coupling_psi) {
+  *scalar_source->get() *= first_coupling_psi + second_coupling_psi * psi.get();
+}
+
 void compute_scalar_curvature_source(
     const gsl::not_null<Scalar<DataVector>*> scalar_source,
     const Scalar<DataVector>& weyl_electric_scalar,
     const Scalar<DataVector>& weyl_magnetic_scalar,
     const Scalar<DataVector>& psi, const double first_coupling_psi,
-    const double second_coupling_psi) {
+    const double second_coupling_psi, const double mass_psi) {
   // Make sure it has the same size
   *scalar_source = make_with_value<Scalar<DataVector>>(psi, 0.);
   // Compute the Riemann squared scalar in vacuum
   scalar_source->get() =
       8.0 * (weyl_electric_scalar.get() - weyl_magnetic_scalar.get());
   // Multiply by the source coupling function
-  scalar_source->get() *= get(
-      coupling_function_prime(psi, first_coupling_psi, second_coupling_psi));
+  multiply_by_coupling_function_prime(scalar_source, psi, first_coupling_psi,
+                                      second_coupling_psi);
+  // Add mass term
+  scalar_source->get() += square(mass_psi) * psi.get();
 }
 
 }  // namespace CurvedScalarWave::Sources
