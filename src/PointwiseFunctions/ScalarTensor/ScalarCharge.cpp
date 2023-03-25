a75ee4b02e6b7497363100fb14ef7cb0f23d55ee
Guillermo Lara <guillermo.lara@black-holes.org>
2023-03-25 13:40:31 +0000

    Add details

--- src/PointwiseFunctions/ScalarTensor/ScalarCharge.cpp
+++ src/PointwiseFunctions/ScalarTensor/ScalarCharge.cpp
@@ -3,6 +3,8 @@
 
 #include "PointwiseFunctions/ScalarTensor/ScalarCharge.hpp"
 
+#include <cmath>
+
 template <typename Frame>
 void ScalarTensor::scalar_charge_integrand(
     const gsl::not_null<Scalar<DataVector>*> result,
@@ -14,12 +16,10 @@ void ScalarTensor::scalar_charge_integrand(
     component = 0.0;
   }
   // Project the scalar gradient on the normal vector
-  get(*result) = phi.get(i) * get<0>(unit_normal_vector);
+  get(*result) = phi.get(0) * get<0>(unit_normal_vector);
   for (size_t j = 1; j < 3; ++j) {
     get(*result) += phi.get(j) * unit_normal_vector.get(j);
   }
-  // Multiply by area element (Need this?)
-  get(*result) *= area_element.get();
   // Multiply by integral prefactor
   get(*result) /= -4.0 * M_PI;
 }
--- src/PointwiseFunctions/ScalarTensor/ScalarCharge.hpp
+++ src/PointwiseFunctions/ScalarTensor/ScalarCharge.hpp
@@ -3,32 +3,66 @@
 
 #pragma once
 
+#include "ApparentHorizons/StrahlkorperGr.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+
+/// \cond
+class DataVector;
+template <typename Frame>
+class Strahlkorper;
+template <typename X, typename Symm, typename IndexList>
+class Tensor;
+
+namespace gsl {
+template <typename>
+struct not_null;
+}  // namespace gsl
+/// \endcond
+
 namespace ScalarTensor {
 
+/// @{
+/*!
+ * \brief Computes the dot product between the scalar gradient and unit normal
+ * the surface.
+ *
+ * \details Compute the integrand of:
+ * \f{align*}
+ * q = - \dfrac{1}{4 \pi} \oint dA \Phi_i n^{i},
+ * \f}
+ * where \f$ n^{i} \f$ is the unit (outward?) normal of the surface.
+ *
+ * This makes sense if \f$ \psi \sim \phi_\infty + q / r + \cdots \f$, for which
+ * \f$ \partial_r \psi \sim  - q / r^2 + \cdots \f$, and thus
+ * \f$ q \sim - r^2 \partial_r \psi  \f$.
+ *
+ */
 template <typename Frame>
 void scalar_charge_integrand(
     const gsl::not_null<Scalar<DataVector>*> result,
     const tnsr::i<DataVector, 3, Frame>& phi,
     const tnsr::I<DataVector, 3, Frame>& unit_normal_vector,
     const Scalar<DataVector>& area_element);
+/// @}
 
 } // namespace ScalarTensor
 
 namespace ScalarTensor::StrahlkorperScalar::Tags {
 
 /// Scalar charge tag.
-struct ScalarCharge : db::SimpleTag {
+struct ScalarChargeIntegrand : db::SimpleTag {
   using type = Scalar<DataVector>;
 };
 
 /// Calculates the scalar charge.
 template <typename Frame>
-struct ScalarChargeCompute : ScalarCharge, db::ComputeTag {
-  using base = ScalarCharge;
+struct ScalarChargeIntegrandCompute : ScalarChargeIntegrand, db::ComputeTag {
+  using base = ScalarChargeIntegrand;
   static constexpr auto function = static_cast<void (*)(
-      gsl::not_null<Scalar<DataVector>*>, const tnsr::i<DataVector, 3, Frame>&,
-      const tnsr::I<DataVector, 3, Frame>&, const Scalar<DataVector>&)>(
-      &StrahlkorperGr::scalar_charge<Frame>);
+      gsl::not_null<Scalar<DataVector>*>,
+      const tnsr::i<DataVector, 3_st, Frame>&,
+      const tnsr::I<DataVector, 3_st, Frame>&, const Scalar<DataVector>&)>(
+      &ScalarTensor::scalar_charge_integrand<Frame>);
   using argument_tags =
       tmpl::list<CurvedScalarWave::Tags::Phi<3_st>,
                  StrahlkorperTags::UnitNormalVector<Frame>, AreaElement<Frame>>;
