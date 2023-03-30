681118eb031d9542f1730c9b783355defebb3dbc
Guillermo Lara <guillermo.lara@black-holes.org>
2023-03-30 15:35:14 +0000

    Add ghst factory details

--- src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/Factory.hpp
@@ -2,3 +2,16 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/MinkowskiZeroScalar.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace GeneralizedHarmonic::Solutions {
+/// ScalarTensor solutions wrapped for GH
+namespace ScalarTensor {
+/// \brief List of all analytic solutions
+using all_solutions = tmpl::list<GeneralizedHarmonic::Solutions::WrappedGr<
+    ::ScalarTensor::Solutions::MinkowskiZeroScalar>>;
+}  // namespace ScalarTensor
+}  // namespace GeneralizedHarmonic::Solutions
