cef072880909e04d45fb30cd4431c843a5b618c1
Guillermo Lara <guillermo.lara@black-holes.org>
2023-03-30 15:30:44 +0000

    Add common solution details

--- src/PointwiseFunctions/AnalyticSolutions/ScalarTensor/Solutions.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/ScalarTensor/Solutions.hpp
@@ -2,3 +2,24 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Solutions.hpp"
+
+namespace ScalarTensor {
+/// Base struct for properties common to all Scalar Tensor analytic solutions
+struct AnalyticSolution {
+  static constexpr size_t volume_dim = 3_st;
+
+  template <typename DataType>
+  using tags = tmpl::push_back<
+      typename gr::AnalyticSolution<3>::template tags<DataType>>;
+};
+
+/*!
+ * \ingroup AnalyticSolutionsGroup
+ * \brief Holds classes implementing a solution to the ScalarTensor system.
+ */
+namespace Solutions {}
+}  // namespace ScalarTensor
