91817f56f42150cd49ac9787848dd50b61d0f4c7
Guillermo Lara <guillermo.lara@black-holes.org>
2023-05-17 15:54:09 +0000

    Add system file

---
+++ src/Evolution/Systems/ScalarTensor/System.hpp
@@ -0,0 +1,9 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+/*!
+ * \ingroup EvolutionSystemsGroup
+ * \brief Items related to evolving the first-order scalar tensor system.
+ */
