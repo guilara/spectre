b5380d25927883f49f058698bba8dd642be32b03
Guillermo Lara <guillermo.lara@black-holes.org>
2023-05-17 15:40:03 +0000

    Add trace reversed stress energy tags

--- src/Evolution/Systems/ScalarTensor/Tags.hpp
+++ src/Evolution/Systems/ScalarTensor/Tags.hpp
@@ -6,8 +6,17 @@
 
 #pragma once
 
+#include "DataStructures/DataBox/DataBoxTag.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Utilities/TMPL.hpp"
 namespace ScalarTensor {
 namespace Tags {
-
+/// Represents the trace reversed stress-energy tensor of the scalar
+/// sector of the ScalarTensor system
+struct TraceReversedStressEnergy : db::SimpleTag {
+  using type = tnsr::aa<DataVector, 3>;
+};
 } // namespace Tags
 } // namespace ScalarTensor
