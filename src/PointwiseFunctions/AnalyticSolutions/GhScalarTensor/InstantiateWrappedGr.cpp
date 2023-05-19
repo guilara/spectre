134b3e89a03a97f5d6eefa0ae35cde5708703467
Guillermo Lara <guillermo.lara@black-holes.org>
2023-05-19 18:23:30 +0000

    Add GhScalarTensor details

--- src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/InstantiateWrappedGr.cpp
@@ -1,2 +1,15 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSchildScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/MinkowskiZeroScalar.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+
+GENERATE_INSTANTIATIONS(WRAPPED_GR_INSTANTIATE,
+                        (ScalarTensor::Solutions::MinkowskiZeroScalar,
+                         ScalarTensor::Solutions::KerrSchildScalar,
+                         ScalarTensor::Solutions::KerrSphericalHarmonic))
