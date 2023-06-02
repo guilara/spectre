abf4f71ebf8cf7d00e6fc159e99a3f4469da6eef
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-02 17:20:28 +0000

    Add stress energy decl

--- src/PointwiseFunctions/AnalyticSolutions/ScalarTensor/StressEnergy.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/ScalarTensor/StressEnergy.cpp
@@ -1,2 +1,28 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
+
+namespace ScalarTensor {
+template <typename DataType>
+void energy_density(gsl::not_null<Scalar<DataType>*> result,
+                    const Scalar<DataType>& scalar_psi,
+                    const tnsr::i<DataType, 3>& scalar_phi,
+                    const Scalar<DataType>& scalar_pi) {
+  //
+}
+
+template <typename DataType>
+void momentum_density(gsl::not_null<tnsr::I<DataType, 3>*> result,
+                      const Scalar<DataType>& scalar_psi,
+                      const tnsr::i<DataType, 3>& scalar_phi,
+                      const Scalar<DataType>& scalar_pi) {
+  //
+}
+
+template <typename DataType>
+void stress_trace(gsl::not_null<Scalar<DataType>*> result,
+                  const Scalar<DataType>& scalar_psi,
+                  const tnsr::i<DataType, 3>& scalar_phi,
+                  const Scalar<DataType>& scalar_pi) {
+  //
+}
+}  // namespace ScalarTensor
--- src/PointwiseFunctions/AnalyticSolutions/ScalarTensor/StressEnergy.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/ScalarTensor/StressEnergy.hpp
@@ -6,3 +6,26 @@
 // Here we write the functions computing the scalar stress-energy tensor
 // projections for the initial data solves (i.e. energy density, momentum
 // density, and trace of the spatial stress-tensor
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace ScalarTensor {
+template <typename DataType>
+void energy_density(gsl::not_null<Scalar<DataType>*> result,
+                    const Scalar<DataType>& scalar_psi,
+                    const tnsr::i<DataType, 3>& scalar_phi,
+                    const Scalar<DataType>& scalar_pi);
+
+template <typename DataType>
+void momentum_density(gsl::not_null<tnsr::I<DataType, 3>*> result,
+                      const Scalar<DataType>& scalar_psi,
+                      const tnsr::i<DataType, 3>& scalar_phi,
+                      const Scalar<DataType>& scalar_pi);
+
+template <typename DataType>
+void stress_trace(gsl::not_null<Scalar<DataType>*> result,
+                  const Scalar<DataType>& scalar_psi,
+                  const tnsr::i<DataType, 3>& scalar_phi,
+                  const Scalar<DataType>& scalar_pi);
+}  // namespace ScalarTensor
