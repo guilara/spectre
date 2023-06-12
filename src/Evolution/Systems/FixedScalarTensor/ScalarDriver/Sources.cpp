fc34ded936d243347205b00d43e98e686c29e6a6
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-12 15:48:40 +0000

    Add scalar driver source definition

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.cpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.cpp
@@ -2,3 +2,13 @@
 // See LICENSE.txt for details.
 
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.hpp"
+
+namespace fe::ScalarDriver::Sources {
+
+void compute_scalar_driver_source(const gsl::not_null<return_type*> result,
+                                  const Scalar<DataVector>& psi,
+                                  const Scalar<DataVector>& target_psi) {
+  // Essentially psi - target_psi
+}
+
+}  // namespace fe::ScalarDriver::Sources
