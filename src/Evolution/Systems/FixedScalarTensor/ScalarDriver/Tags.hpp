997e6839c27cf86c27ef197f6acc92bc8d3f6670
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-12 14:07:43 +0000

    Add new system files

---
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp
@@ -0,0 +1,10 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace fe::ScalarDriver {
+
+namespace Tags {}  // namespace Tags
+
+}  // namespace fe::ScalarDriver
