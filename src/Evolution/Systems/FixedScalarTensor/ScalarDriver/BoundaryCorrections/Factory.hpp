2336b3ec66ef4daff5ba0824c9ed7a0b6bdbd5d9
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-13 15:42:45 +0000

    Add boundary correction factory

---
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryCorrections/Factory.hpp
@@ -0,0 +1,7 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryCorrections/UpwindPenalty.hpp"
