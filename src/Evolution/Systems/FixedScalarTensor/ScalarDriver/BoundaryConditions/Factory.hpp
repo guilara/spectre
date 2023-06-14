e2b72c052cd43034d8b37e0dc364da4dfc3b0fd1
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-14 12:53:28 +0000

    Add boundary condition factory

---
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
@@ -0,0 +1,19 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "Domain/BoundaryConditions/Periodic.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/ConstraintPreservingSphericalRadiation.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/DemandOutgoingCharSpeeds.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace fe::ScalarDriver::BoundaryConditions {
+/// Typelist of standard BoundaryConditions
+template <size_t Dim>
+using standard_boundary_conditions =
+    tmpl::list<DemandOutgoingCharSpeeds,
+               domain::BoundaryConditions::Periodic<BoundaryCondition>>;
+}  // namespace fe::ScalarDriver::BoundaryConditions
