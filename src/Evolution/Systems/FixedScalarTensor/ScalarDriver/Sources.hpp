429186267446aa4953c578a7c37b7fd1308510c2
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-14 14:09:56 +0000

    Change target source

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.hpp
@@ -5,6 +5,15 @@
 
 #include "DataStructures/DataBox/Tag.hpp"
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+//
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "Evolution/Systems/ScalarTensor/Sources/Tags.hpp"
+#include "Evolution/Systems/ScalarTensor/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/WeylElectric.hpp"
+#include "PointwiseFunctions/GeneralRelativity/WeylMagnetic.hpp"
+#include "Utilities/Gsl.hpp"
+//
 #include "Options/Options.hpp"
 
 namespace fe::ScalarDriver::Sources {
@@ -51,11 +60,22 @@ struct ScalarDriverSourceCompute : ScalarDriverSource, db::ComputeTag {
  */
 template <typename Frame, typename DataType>
 struct TargetPsiCompute : TargetPsi, db::ComputeTag {
-  using argument_tags = tmpl::list<fe::ScalarDriver::Psi>;
+  //   using argument_tags = tmpl::list<fe::ScalarDriver::Psi>;
+  using argument_tags =
+      tmpl::list<gr::Tags::WeylElectricScalar<DataType>,
+                 gr::Tags::WeylMagneticScalar<DataType>,
+                 CurvedScalarWave::Tags::Psi,
+                 ScalarTensor::Sources::Tags::ScalarFirstCouplingParameter,
+                 ScalarTensor::Sources::Tags::ScalarSecondCouplingParameter,
+                 ScalarTensor::Sources::Tags::ScalarMass>;
   using return_type = Scalar<DataVector>;
+  //   static constexpr void (*function)(const gsl::not_null<return_type*>
+  //   result,
+  //                                     const Scalar<DataVector>&) =
+  //       &compute_target_psi;
   static constexpr void (*function)(const gsl::not_null<return_type*> result,
                                     const Scalar<DataVector>&) =
-      &compute_target_psi;
+      &ScalarTensor::Sources::compute_scalar_curvature_source;
   using base = ScalarSource;
 };
 
