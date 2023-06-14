3cdc919bd69a304fa2b3e076a3fc421996420965
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-14 12:51:41 +0000

    Add details

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/DemandOutgoingCharSpeeds.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/DemandOutgoingCharSpeeds.hpp
@@ -2,3 +2,84 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <memory>
+#include <optional>
+#include <pup.h>
+#include <string>
+#include <type_traits>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/FaceNormal.hpp"
+#include "Evolution/BoundaryConditions/Type.hpp"
+#include "Evolution/Systems/CurvedScalarWave/BoundaryConditions/BoundaryCondition.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/BoundaryCondition.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/Serialization/CharmPupable.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+namespace domain::Tags {
+template <size_t Dim, typename Frame>
+struct Coordinates;
+}  // namespace domain::Tags
+/// \endcond
+
+namespace fe::ScalarDriver::BoundaryConditions {
+/// A `BoundaryCondition` that only verifies that all characteristic speeds are
+/// directed out of the domain; no boundary data is altered by this boundary
+/// condition.
+class DemandOutgoingCharSpeeds final : public BoundaryCondition {
+ public:
+  using options = tmpl::list<>;
+  static constexpr Options::String help{
+      "Boundary conditions which check that all characteristic "
+      "fields are outflowing."};
+  DemandOutgoingCharSpeeds() = default;
+  /// \cond
+  DemandOutgoingCharSpeeds(DemandOutgoingCharSpeeds&&) = default;
+  DemandOutgoingCharSpeeds& operator=(DemandOutgoingCharSpeeds&&) = default;
+  DemandOutgoingCharSpeeds(const DemandOutgoingCharSpeeds&) = default;
+  DemandOutgoingCharSpeeds& operator=(const DemandOutgoingCharSpeeds&) =
+      default;
+  /// \endcond
+  ~DemandOutgoingCharSpeeds() override = default;
+
+  explicit DemandOutgoingCharSpeeds(CkMigrateMessage* msg);
+
+  WRAPPED_PUPable_decl_base_template(
+      domain::BoundaryConditions::BoundaryCondition, DemandOutgoingCharSpeeds);
+
+  auto get_clone() const -> std::unique_ptr<
+      domain::BoundaryConditions::BoundaryCondition> override;
+
+  static constexpr evolution::BoundaryConditions::Type bc_type =
+      evolution::BoundaryConditions::Type::DemandOutgoingCharSpeeds;
+
+  void pup(PUP::er& p) override;
+
+  using dg_interior_evolved_variables_tags = tmpl::list<>;
+  using dg_interior_temporary_tags =
+      tmpl::list<Tags::ConstraintGamma1, gr::Tags::Lapse<DataVector>,
+                 gr::Tags::Shift<DataVector, Dim>>;
+  using dg_interior_dt_vars_tags = tmpl::list<>;
+  using dg_interior_deriv_vars_tags = tmpl::list<>;
+  using dg_gridless_tags = tmpl::list<>;
+
+  std::optional<std::string> dg_demand_outgoing_char_speeds(
+      const std::optional<tnsr::I<DataVector, Dim, Frame::Inertial>>&
+          face_mesh_velocity,
+      const tnsr::i<DataVector, Dim>& normal_covector,
+      const tnsr::I<DataVector, Dim>& normal_vector,
+      const Scalar<DataVector>& gamma1, const Scalar<DataVector>& lapse,
+      const tnsr::I<DataVector, Dim>& shift) const {
+    // Use the boundary condition from CurvedScalarWave
+    CurvedScalarWave::fe::ScalarDriver::BoundaryConditions::
+        DemandOutgoingCharSpeeds<3_st>::dg_demand_outgoing_char_speeds(
+            face_mesh_velocity, normal_covector, normal_vector, gamma1, lapse,
+            shift);
+  }
+};
+}  // namespace fe::ScalarDriver::BoundaryConditions
