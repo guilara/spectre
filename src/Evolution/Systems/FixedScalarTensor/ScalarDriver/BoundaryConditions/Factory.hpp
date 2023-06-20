b247247df9984837ac86356ce11195bb450bb32e
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-20 15:16:44 +0000

    Add boundary condition to factory list

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
@@ -7,6 +7,7 @@
 
 #include "Domain/BoundaryConditions/Periodic.hpp"
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/ConstraintPreservingSphericalRadiation.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/AnalyticConstant.hpp"
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/DemandOutgoingCharSpeeds.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -14,5 +15,6 @@ namespace fe::ScalarDriver::BoundaryConditions {
 /// Typelist of standard BoundaryConditions
 using standard_boundary_conditions =
     tmpl::list<DemandOutgoingCharSpeeds,
+               AnalyticConstant,
                domain::BoundaryConditions::Periodic<BoundaryCondition>>;
 }  // namespace fe::ScalarDriver::BoundaryConditions
