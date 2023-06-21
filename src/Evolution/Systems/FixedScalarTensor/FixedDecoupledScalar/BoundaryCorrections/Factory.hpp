db16723d209bc612de80b71f6801ea082aaf5853
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-21 14:50:34 +0000

    Add BCorr factory file

---
+++ src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/Factory.hpp
@@ -0,0 +1,7 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/ProductOfCorrections.hpp"
