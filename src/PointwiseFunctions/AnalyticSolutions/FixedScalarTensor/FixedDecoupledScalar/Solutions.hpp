14db2e22ebe14efc665855306eb28e218f06196c
Guillermo Lara <guillermo.lara@black-holes.org>
2023-06-22 07:58:43 +0000

    Add analytic solution details

---
+++ src/PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/Solutions.hpp
@@ -0,0 +1,30 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/Solutions.hpp"
+
+namespace fe::DecoupledScalar {
+/// Base struct for properties common to all FixedDecoupledScalar analytic
+/// solutions
+struct AnalyticSolution {
+  static constexpr size_t volume_dim = 3_st;
+
+  template <typename DataType>
+  using tags = tmpl::push_back<
+      typename ScalarTensor::AnalyticSolution::template tags<DataType>,
+      // Add scalar driver variables here
+      fe::ScalarDriver::Tags::Psi, fe::ScalarDriver::Tags::Pi,
+      fe::ScalarDriver::Tags::Phi<3_st> >;
+};
+
+/*!
+ * \ingroup AnalyticSolutionsGroup
+ * \brief Holds classes implementing a solution to the ScalarTensor system.
+ */
+namespace Solutions {}
+}  // namespace fe::DecoupledScalar
