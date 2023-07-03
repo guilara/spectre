23143ca2b034823a9a8fe11982c0d5bdfdd1dd25
Guillermo Lara <guillermo.lara@black-holes.org>
2023-07-03 14:14:47 +0000

    Add driver system details

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Constraints.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Constraints.hpp
@@ -2,3 +2,57 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Constraints.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace fe::ScalarDriver {
+
+namespace Tags {
+/*!
+ * \brief Compute item to get the one-index constraint for the scalar driver
+ * evolution system.
+ *
+ * \details See `one_index_constraint()`. Can be retrieved using
+ * `CurvedScalarWave::Tags::OneIndexConstraint`.
+ */
+struct OneIndexConstraintCompute : OneIndexConstraint, db::ComputeTag {
+  using argument_tags =
+      tmpl::list<::Tags::deriv<Psi, tmpl::size_t<3_st>, Frame::Inertial>,
+                 Phi<3_st>>;
+  using return_type = tnsr::i<DataVector, 3_st, Frame::Inertial>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result,
+      const tnsr::i<DataVector, 3_st, Frame::Inertial>&,
+      const tnsr::i<DataVector, 3_st, Frame::Inertial>&) =
+      &CurvedScalarWave::one_index_constraint<3_st>;
+  using base = OneIndexConstraint;
+};
+
+/*!
+ * \brief Compute item to get the two-index constraint for the scalar driver
+ * evolution system.
+ *
+ * \details See `two_index_constraint()`. Can be retrieved using
+ * `CurvedScalarWave::Tags::TwoIndexConstraint`.
+ */
+struct TwoIndexConstraintCompute : TwoIndexConstraint, db::ComputeTag {
+  using argument_tags =
+      tmpl::list<::Tags::deriv<Phi<3_st>, tmpl::size_t<3_st>, Frame::Inertial>>;
+  using return_type = tnsr::ij<DataVector, 3_st, Frame::Inertial>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result,
+      const tnsr::ij<DataVector, 3_st, Frame::Inertial>&) =
+      &CurvedScalarWave::two_index_constraint<3_st>;
+  using base = TwoIndexConstraint;
+};
+}  // namespace Tags
+
+}  // namespace fe::ScalarDriver
--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.cpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.cpp
@@ -1,2 +1,41 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
+
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Sources.hpp"
+
+namespace fe::ScalarDriver::Sources {
+
+void add_scalar_driver_source_to_dt_pi_scalar(
+    gsl::not_null<Scalar<DataVector>*> dt_pi,
+    const Scalar<DataVector>& scalar_driver_source,
+    const Scalar<DataVector>& lapse) {
+  dt_pi->get() += get(lapse) * scalar_driver_source.get();
+}
+
+void add_scalar_driver_friction_term_to_dt_pi_scalar(
+    gsl::not_null<Scalar<DataVector>*> dt_pi,
+    const Scalar<DataVector>& scalar_driver_pi, const Scalar<DataVector>& lapse,
+    const tnsr::I<DataVector, 3_st>& shift, const double scalar_tau_parameter,
+    const double scalar_sigma_parameter) {
+  dt_pi->get() += (scalar_tau_parameter / scalar_sigma_parameter) *
+                  square(lapse.get()) * scalar_driver_pi.get();
+}
+
+void compute_scalar_driver_source(
+    const gsl::not_null<Scalar<DataVector>*> scalar_driver_source,
+    const Scalar<DataVector>& psi, const Scalar<DataVector>& target_psi,
+    const double scalar_tau_parameter, const double scalar_sigma_parameter) {
+  // Make sure it has the same size
+  *scalar_driver_source = make_with_value<Scalar<DataVector>>(psi, 0.);
+  scalar_driver_source->get() =
+      -(1.0 / scalar_sigma_parameter) * (psi.get() - target_psi.get());
+}
+
+void compute_target_psi(const gsl::not_null<Scalar<DataVector>*> target_psi,
+                        const Scalar<DataVector>& psi) {
+  // Make sure it has the same size
+  *target_psi = make_with_value<Scalar<DataVector>>(psi, 0.);
+  target_psi->get() = psi.get();
+}
+
+}  // namespace fe::ScalarDriver::Sources
