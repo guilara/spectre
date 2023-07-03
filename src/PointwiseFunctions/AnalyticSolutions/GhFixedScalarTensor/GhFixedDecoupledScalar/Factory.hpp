e863129328d261f3d985db2a166ae95eaffa3618
Guillermo Lara <guillermo.lara@black-holes.org>
2023-07-03 15:00:45 +0000

    Add details

--- src/PointwiseFunctions/AnalyticSolutions/GhFixedScalarTensor/GhFixedDecoupledScalar/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhFixedScalarTensor/GhFixedDecoupledScalar/Factory.hpp
@@ -2,3 +2,22 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/KerrSchildScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace gh::Solutions {
+/// ScalarTensor solutions wrapped for GH
+namespace fe::DecoupledScalar {
+/// \brief List of all analytic solutions
+using all_solutions = tmpl::list<gh::Solutions::WrappedGr<
+    ::fe::DecoupledScalar::Solutions::MinkowskiZeroScalar>,
+    gh::Solutions::WrappedGr<
+    ::fe::DecoupledScalar::Solutions::KerrSchildScalar>,
+    gh::Solutions::WrappedGr<
+    ::fe::DecoupledScalar::Solutions::KerrSphericalHarmonic>>;
+}  // namespace fe::DecoupledScalar
+}  // namespace gh::Solutions
--- src/PointwiseFunctions/AnalyticSolutions/GhFixedScalarTensor/GhFixedDecoupledScalar/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhFixedScalarTensor/GhFixedDecoupledScalar/InstantiateWrappedGr.cpp
@@ -1,2 +1,15 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/KerrSchildScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
+#include "Utilities/GenerateInstantiations.hpp"
+
+GENERATE_INSTANTIATIONS(WRAPPED_GR_INSTANTIATE,
+                        (fe::DecoupledScalar::Solutions::MinkowskiZeroScalar,
+                        fe::DecoupledScalar::Solutions::KerrSchildScalar,
+                        fe::DecoupledScalar::Solutions::KerrSphericalHarmonic))
