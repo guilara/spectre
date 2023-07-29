949682680b403c755d96e2ee13083db4a0f465b0
Guillermo Lara <guillermo.lara@black-holes.org>
2023-07-29 10:24:11 +0000

    Fix

--- src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/Factory.hpp
@@ -4,9 +4,7 @@
 #pragma once
 
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
-#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSchildScalar.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSphericalHarmonic.hpp"
-#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/MinkowskiZeroScalar.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace gh::Solutions {
@@ -14,8 +12,6 @@ namespace gh::Solutions {
 namespace ScalarTensor {
 /// \brief List of all analytic solutions
 using all_solutions = tmpl::list<
-    gh::Solutions::WrappedGr<::ScalarTensor::Solutions::MinkowskiZeroScalar>,
-    gh::Solutions::WrappedGr<::ScalarTensor::Solutions::KerrSchildScalar>,
     gh::Solutions::WrappedGr<::ScalarTensor::Solutions::KerrSphericalHarmonic>>;
 }  // namespace ScalarTensor
 }  // namespace gh::Solutions
--- src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/InstantiateWrappedGr.cpp
@@ -4,12 +4,8 @@
 #include "DataStructures/DataBox/Prefixes.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
-#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSchildScalar.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSphericalHarmonic.hpp"
-#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/MinkowskiZeroScalar.hpp"
 #include "Utilities/GenerateInstantiations.hpp"
 
 GENERATE_INSTANTIATIONS(WRAPPED_GR_INSTANTIATE,
-                        (ScalarTensor::Solutions::MinkowskiZeroScalar,
-                         ScalarTensor::Solutions::KerrSchildScalar,
-                         ScalarTensor::Solutions::KerrSphericalHarmonic))
+                        (ScalarTensor::Solutions::KerrSphericalHarmonic))
