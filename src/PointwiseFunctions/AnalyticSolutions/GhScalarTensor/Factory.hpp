15e18ce7da78589daf39c98948e4506d7be16543
Guillermo Lara <guillermo.lara@black-holes.org>
2023-07-29 10:11:53 +0000

    Add details

--- src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhScalarTensor/Factory.hpp
@@ -2,3 +2,20 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSchildScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/ScalarTensor/MinkowskiZeroScalar.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace gh::Solutions {
+/// ScalarTensor solutions wrapped for GH
+namespace ScalarTensor {
+/// \brief List of all analytic solutions
+using all_solutions = tmpl::list<
+    gh::Solutions::WrappedGr<::ScalarTensor::Solutions::MinkowskiZeroScalar>,
+    gh::Solutions::WrappedGr<::ScalarTensor::Solutions::KerrSchildScalar>,
+    gh::Solutions::WrappedGr<::ScalarTensor::Solutions::KerrSphericalHarmonic>>;
+}  // namespace ScalarTensor
+}  // namespace gh::Solutions
