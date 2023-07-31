362f07d42e4b76f6ef0e34faead767ae1a85606e
Guillermo Lara <guillermo.lara@black-holes.org>
2023-07-31 13:40:46 +0000

    Add initial data details

--- src/PointwiseFunctions/AnalyticData/GhScalarTensor/Factory.hpp
+++ src/PointwiseFunctions/AnalyticData/GhScalarTensor/Factory.hpp
@@ -2,3 +2,16 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include "PointwiseFunctions/AnalyticData/ScalarTensor/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace gh::AnalyticData {
+/// ScalarTensor solutions wrapped for GH
+namespace ScalarTensor {
+/// \brief List of all analytic solutions
+using all_analytic_data = tmpl::list<gh::Solutions::WrappedGr<
+    ::ScalarTensor::AnalyticData::KerrSphericalHarmonic>>;
+}  // namespace ScalarTensor
+}  // namespace gh::AnalyticData
