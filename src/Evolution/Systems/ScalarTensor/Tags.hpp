4bb85e77fb9e45fcb60dcbd4c9a318800dcb34f6
Guillermo Lara <guillermo.lara@black-holes.org>
2023-08-08 16:53:23 +0000

    Add compute tags

--- src/Evolution/Systems/ScalarTensor/Tags.hpp
+++ src/Evolution/Systems/ScalarTensor/Tags.hpp
@@ -68,6 +68,48 @@ struct CSW : db::PrefixTag, Tag {
   static std::string name() { return "CSW(" + Tag::name() + ")"; }
 };
 
+/*!
+ * \brief Compute tag for observing the evolved variable Psi.
+ */
+struct CSWPsiCompute : CSW<CurvedScalarWave::Tags::Psi>, db::ComputeTag {
+  using argument_tags = tmpl::list<CurvedScalarWave::Tags::Psi>;
+  using return_type = Scalar<DataVector>;
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const Scalar<DataVector>& input) {
+    get(*result) = get(input);
+  }
+  using base = CSW<CurvedScalarWave::Tags::Psi>;
+};
+
+/*!
+ * \brief Compute tag for observing the evolved variable Pi.
+ */
+struct CSWPiCompute : CSW<CurvedScalarWave::Tags::Pi>, db::ComputeTag {
+  using argument_tags = tmpl::list<CurvedScalarWave::Tags::Pi>;
+  using return_type = Scalar<DataVector>;
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const Scalar<DataVector>& input) {
+    get(*result) = get(input);
+  }
+  using base = CSW<CurvedScalarWave::Tags::Pi>;
+};
+
+/*!
+ * \brief Compute tag for observing the evolved variable Phi.
+ */
+template <size_t Dim>
+struct CSWPhiCompute : CSW<CurvedScalarWave::Tags::Phi<Dim>>, db::ComputeTag {
+  using argument_tags = tmpl::list<CurvedScalarWave::Tags::Phi<Dim>>;
+  using return_type = tnsr::i<DataVector, Dim>;
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const tnsr::i<DataVector, Dim>& input) {
+    for (size_t i = 0; i < Dim; ++i) {
+      result->get(i) = input.get(i);
+    }
+  }
+  using base = CSW<CurvedScalarWave::Tags::Phi<Dim>>;
+};
+
 }  // namespace Tags
 
 }  // namespace ScalarTensor
