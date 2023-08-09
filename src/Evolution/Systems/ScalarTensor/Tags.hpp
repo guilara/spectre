5c70f02abb084ae397ad89e5151259c2df1aca8d
Guillermo Lara <guillermo.lara@black-holes.org>
2023-08-09 09:46:29 +0000

    Add details

--- src/Evolution/Systems/ScalarTensor/Tags.hpp
+++ src/Evolution/Systems/ScalarTensor/Tags.hpp
@@ -7,6 +7,9 @@
 #include "DataStructures/DataBox/Tag.hpp"
 #include "DataStructures/DataVector.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Constraints.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
 #include "Options/String.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -59,93 +62,104 @@ struct ScalarMass : db::SimpleTag {
   static double create_from_options(const double mass_psi) { return mass_psi; }
 };
 
-/// We use this prefix tag to avoid ambiguities with the ::gh system when
-/// parsing observed variables
+/*!
+ * \brief Prefix tag to avoid ambiguities when observing variables with the same
+ * name in both parent systems.
+ * \note Since we also add compute tags for these quantities, we do not make
+ * this a derived class of `Tag`. Otherwise, we would have tags with repeated
+ * base tags in the `ObservationBox`.
+ */
 template <typename Tag>
-struct CSW : db::PrefixTag, Tag {
+struct CSW : db::PrefixTag, db::SimpleTag {
   using type = typename Tag::type;
   using tag = Tag;
 };
 
 /*!
  * \brief Compute tag for observing the evolved variable Psi.
+ * \details This tag copies the data from the evolved variable Tag
+ * CurvedScalarWave::Tags::Psi into the wrapped tag used for observation.
  */
 struct CSWPsiCompute : CSW<CurvedScalarWave::Tags::Psi>, db::ComputeTag {
   using argument_tags = tmpl::list<CurvedScalarWave::Tags::Psi>;
   using return_type = Scalar<DataVector>;
   static constexpr void function(const gsl::not_null<return_type*> result,
-                                    const Scalar<DataVector>& input) {
-    get(*result) = get(input);
+                                    const Scalar<DataVector>& psi) {
+    get(*result) = get(psi);
   }
   using base = CSW<CurvedScalarWave::Tags::Psi>;
 };
 
 /*!
  * \brief Compute tag for observing the evolved variable Pi.
+ * \details This tag copies the data from the evolved variable Tag
+ * CurvedScalarWave::Tags::Pi into the wrapped tag used for observation.
  */
 struct CSWPiCompute : CSW<CurvedScalarWave::Tags::Pi>, db::ComputeTag {
   using argument_tags = tmpl::list<CurvedScalarWave::Tags::Pi>;
   using return_type = Scalar<DataVector>;
   static constexpr void function(const gsl::not_null<return_type*> result,
-                                    const Scalar<DataVector>& input) {
-    get(*result) = get(input);
+                                    const Scalar<DataVector>& pi) {
+    get(*result) = get(pi);
   }
   using base = CSW<CurvedScalarWave::Tags::Pi>;
 };
 
 /*!
  * \brief Compute tag for observing the evolved variable Phi.
+ * \details This tag copies the data from the evolved variable Tag
+ * CurvedScalarWave::Tags::Phi into the wrapped tag used for observation.
  */
 template <size_t Dim>
 struct CSWPhiCompute : CSW<CurvedScalarWave::Tags::Phi<Dim>>, db::ComputeTag {
   using argument_tags = tmpl::list<CurvedScalarWave::Tags::Phi<Dim>>;
   using return_type = tnsr::i<DataVector, Dim>;
   static constexpr void function(const gsl::not_null<return_type*> result,
-                                    const tnsr::i<DataVector, Dim>& input) {
+                                    const tnsr::i<DataVector, Dim>& phi) {
     for (size_t i = 0; i < Dim; ++i) {
-      result->get(i) = input.get(i);
+      result->get(i) = phi.get(i);
     }
   }
   using base = CSW<CurvedScalarWave::Tags::Phi<Dim>>;
 };
 
 /*!
- * \brief Compute tag for observing the scalar one index constraint.
+ * \brief Computes the scalar-wave one-index constraint.
+ * \details The one-index constraint is assigned to a wrapped tag to avoid
+ * clashes with the ::gh constraints during observation.
  */
 template <size_t Dim>
 struct CSWOneIndexConstraintCompute
     : CSW<CurvedScalarWave::Tags::OneIndexConstraint<Dim>>,
       db::ComputeTag {
   using argument_tags =
-      tmpl::list<CurvedScalarWave::Tags::OneIndexConstraint<Dim>>;
+      tmpl::list<::Tags::deriv<CurvedScalarWave::Tags::Psi, tmpl::size_t<Dim>,
+                               Frame::Inertial>,
+                 CurvedScalarWave::Tags::Phi<Dim>>;
   using return_type = tnsr::i<DataVector, Dim>;
-  static constexpr void function(const gsl::not_null<return_type*> result,
-                                 const tnsr::i<DataVector, Dim>& input) {
-    for (size_t i = 0; i < Dim; ++i) {
-      result->get(i) = input.get(i);
-    }
-  }
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const tnsr::i<DataVector, Dim>&,
+                                    const tnsr::i<DataVector, Dim>&) =
+      &CurvedScalarWave::one_index_constraint<Dim>;
   using base = CSW<CurvedScalarWave::Tags::OneIndexConstraint<Dim>>;
 };
 
 /*!
- * \brief Compute tag for observing the scalar two index constraint.
+ * \brief Computes the scalar-wave two-index constraint.
+ * \details The two-index constraint is assigned to a wrapped tag to avoid
+ * clashes with the ::gh constraints during observation.
  */
 template <size_t Dim>
 struct CSWTwoIndexConstraintCompute
     : CSW<CurvedScalarWave::Tags::TwoIndexConstraint<Dim>>,
       db::ComputeTag {
   using argument_tags =
-      tmpl::list<CurvedScalarWave::Tags::TwoIndexConstraint<Dim>>;
+      tmpl::list<::Tags::deriv<CurvedScalarWave::Tags::Phi<Dim>,
+                               tmpl::size_t<Dim>, Frame::Inertial>>;
   using return_type = tnsr::ij<DataVector, Dim>;
-  static constexpr void function(const gsl::not_null<return_type*> result,
-                                 const tnsr::ij<DataVector, Dim>& input) {
-    for (size_t i = 0; i < Dim; ++i) {
-      for (size_t j = 0; j < Dim; ++j) {
-        result->get(i, j) = input.get(i, j);
-      }
-    }
-  }
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const tnsr::ij<DataVector, Dim>&) =
+      &CurvedScalarWave::two_index_constraint<Dim>;
   using base = CSW<CurvedScalarWave::Tags::TwoIndexConstraint<Dim>>;
 };
 
