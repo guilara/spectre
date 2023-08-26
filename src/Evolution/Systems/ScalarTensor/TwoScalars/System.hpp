d349541b97a1f9e5b759bc064926ec4308a494ce
Guillermo Lara <guillermo.lara@black-holes.org>
2023-08-26 18:04:56 +0000

    Add details

--- src/Evolution/Systems/ScalarTensor/TwoScalars/System.hpp
+++ src/Evolution/Systems/ScalarTensor/TwoScalars/System.hpp
@@ -2,3 +2,57 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "DataStructures/VariablesTag.hpp"
+#include "Evolution/Systems/CurvedScalarWave/System.hpp"
+#include "Evolution/Systems/GeneralizedHarmonic/System.hpp"
+#include "Evolution/Systems/ScalarTensor/TwoScalars/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
+/*!
+ * \ingroup EvolutionSystemsGroup
+ * \brief Items related to evolving the first-order scalar tensor system.
+ */
+namespace ScalarTensor::TwoScalars {
+/*!
+ * \brief Scalar Tensor system obtained from combining two copies of the
+ * CurvedScalarWave and one copy of the gh system.
+ */
+struct System {
+  static constexpr bool has_primitive_and_conservative_vars = false;
+  static constexpr size_t volume_dim = 3;
+
+  using gh_system = gh::System<3_st>;
+  using scalar_system = CurvedScalarWave::System<3_st>;
+
+  template <size_t ScalarLabel>
+  using wrapped_scalar_variables = ::Tags::Variables<
+      db::wrap_tags_in<ScalarTensor::TwoScalar::Tags::Csw,
+                       typename scalar_system::variables_tag::tags_list,
+                       tmpl::size_t<ScalarLabel>>>;
+
+  using variables_tag = ::Tags::Variables<
+      tmpl::append<typename gh_system::variables_tag::tags_list,
+                   typename wrapped_scalar_variables<1>::tags_list,
+                   typename wrapped_scalar_variables<2>::tags_list>>;
+
+  using flux_variables = tmpl::append<typename gh_system::flux_variables,
+                                      typename scalar_system::flux_variables,
+                                      typename scalar_system::flux_variables>;
+
+  using gradient_variables =
+      tmpl::append<typename gh_system::gradient_variables,
+                   typename wrapped_scalar_variables<1>::tags_list,
+                   typename wrapped_scalar_variables<2>::tags_list>;
+  using gradients_tags = gradient_variables;
+
+  static constexpr bool is_in_flux_conservative_form = false;
+
+  using inverse_spatial_metric_tag =
+      typename gh_system::inverse_spatial_metric_tag;
+};
+
+}  // namespace ScalarTensor::TwoScalars
--- src/Evolution/Systems/ScalarTensor/TwoScalars/Tags.hpp
+++ src/Evolution/Systems/ScalarTensor/TwoScalars/Tags.hpp
@@ -2,3 +2,98 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/DataBoxTag.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Constraints.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
+#include "Options/String.hpp"
+#include "Utilities/TMPL.hpp"
+
+/*!
+ * \brief Tags for the scalar tensor system.
+ */
+namespace ScalarTensor::TwoScalars {
+namespace Tags {}  // namespace Tags
+
+namespace OptionTags {}  // namespace OptionTags
+
+namespace Tags {
+
+/*!
+ * \brief Prefix tag to avoid ambiguities when observing variables with the same
+ * name all component systems.
+ * \note Since we also add compute tags for these quantities, we do not make
+ * this a derived class of `Tag`. Otherwise, we would have tags with repeated
+ * base tags in the `ObservationBox`.
+ */
+template <typename Tag, size_t ScalarLabel>
+struct Csw : db::PrefixTag, db::SimpleTag {
+  using type = typename Tag::type;
+  using tag = Tag;
+};
+
+/*!
+ * \brief Computes the scalar-wave one-index constraint.
+ * \details The one-index constraint is assigned to a wrapped tag to avoid
+ * clashes with the ::gh constraints during observation.
+ * \note We do not use ScalarTensor::Tags::CswCompute to retrieve the
+ * CurvedScalarWave constraints since we do not want to add the bare compute
+ * tags (::CurvedScalarWave::Tags::OneIndexConstraintCompute and
+ * ::CurvedScalarWave::Tags::TwoIndexConstraintCompute) directly in the
+ * ObservationBox, since that will make them observable and would lead to a
+ * clash with the ::gh constraint tags.
+ */
+template <size_t ScalarLabel>
+struct CswOneIndexConstraintCompute
+    : Csw<CurvedScalarWave::Tags::OneIndexConstraint<3>>,
+      db::ComputeTag {
+  static constexpr size_t Dim = 3;
+  using argument_tags =
+      tmpl::list<::Tags::deriv<Csw<CurvedScalarWave::Tags::Psi, ScalarLabel>,
+                               tmpl::size_t<Dim>, Frame::Inertial>,
+                 Csw<CurvedScalarWave::Tags::Phi<Dim>, ScalarLabel>>;
+  using return_type = tnsr::i<DataVector, Dim>;
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const tnsr::i<DataVector, Dim>&,
+                                    const tnsr::i<DataVector, Dim>&) =
+      &CurvedScalarWave::one_index_constraint<Dim>;
+  using base =
+      Csw<CurvedScalarWave::Tags::OneIndexConstraint<Dim>, ScalarLabel>;
+};
+
+/*!
+ * \brief Computes the scalar-wave two-index constraint.
+ * \details The two-index constraint is assigned to a wrapped tag to avoid
+ * clashes with the ::gh constraints during observation.
+ * \note We do not use ScalarTensor::Tags::CswCompute to retrieve the
+ * CurvedScalarWave constraints since we do not want to add the bare compute
+ * tags (::CurvedScalarWave::Tags::OneIndexConstraintCompute and
+ * ::CurvedScalarWave::Tags::TwoIndexConstraintCompute) directly in the
+ * ObservationBox, since that will make them observable and would lead to a
+ * clash with the ::gh constraint tags.
+ */
+template <size_t ScalarLabel>
+struct CswTwoIndexConstraintCompute
+    : Csw<CurvedScalarWave::Tags::TwoIndexConstraint<3>, ScalarLabel>,
+      db::ComputeTag {
+  static constexpr size_t Dim = 3;
+  using argument_tags = tmpl::list<
+      ::Tags::deriv<Csw<CurvedScalarWave::Tags::Phi<Dim>, ScalarLabel>,
+                    tmpl::size_t<Dim>, Frame::Inertial>>;
+  using return_type = tnsr::ij<DataVector, Dim>;
+  static constexpr void (*function)(const gsl::not_null<return_type*> result,
+                                    const tnsr::ij<DataVector, Dim>&) =
+      &CurvedScalarWave::two_index_constraint<Dim>;
+  using base =
+      Csw<CurvedScalarWave::Tags::TwoIndexConstraint<Dim>, ScalarLabel>;
+};
+
+}  // namespace Tags
+
+}  // namespace ScalarTensor::TwoScalars
