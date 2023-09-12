c6bb033c8f5e61ea5f1733f3f6ba30b6a8ea24d4
Guillermo Lara <guillermo.lara@black-holes.org>
2023-09-12 08:37:33 +0000

    Fix

--- src/PointwiseFunctions/AnalyticSolutions/GhFixedScalarTensor/GhFixedDecoupledScalar/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GhFixedScalarTensor/GhFixedDecoupledScalar/InstantiateWrappedGr.cpp
@@ -2,14 +2,16 @@
 // See LICENSE.txt for details.
 
 #include "DataStructures/DataBox/Prefixes.hpp"
-#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/KerrSchildScalar.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/FixedScalarTensor/FixedDecoupledScalar/SphericalKerrSchildSH.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
 #include "Utilities/GenerateInstantiations.hpp"
 
 GENERATE_INSTANTIATIONS(WRAPPED_GR_INSTANTIATE,
                         (fe::DecoupledScalar::Solutions::MinkowskiZeroScalar,
-                        fe::DecoupledScalar::Solutions::KerrSchildScalar,
-                        fe::DecoupledScalar::Solutions::KerrSphericalHarmonic))
+                         fe::DecoupledScalar::Solutions::KerrSchildScalar,
+                         fe::DecoupledScalar::Solutions::KerrSphericalHarmonic,
+                         fe::DecoupledScalar::Solutions::SphericalKerrSchildSH))
