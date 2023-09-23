a60890c7e80ea3011082532f9a96d2d9895bb561
Guillermo Lara <glara@aei.mpg.de>
2023-09-23 14:05:23 +0000

    Add constraint gammas option tags

--- src/Evolution/Systems/ScalarTensor/Tags.hpp
+++ src/Evolution/Systems/ScalarTensor/Tags.hpp
@@ -54,6 +54,52 @@ struct ScalarMass {
   static constexpr Options::String help{
       "Mass of the scalar field in code units"};
 };
+
+/*!
+ * \brief Amplitude of the gaussian function for the constraint damping
+ * parameter for the scalar driver equation.
+ */
+struct AmplitudeConstraintGamma2 {
+  static std::string name() {
+    return "AmplitudeConstraintGamma2(CurvedScalarWave)";
+  }
+  using type = double;
+  static constexpr Options::String help{
+      "Amplitude of the gaussian function for the constraint damping parameter "
+      "for the scalar driver equation."};
+  static type lower_bound() { return 0.; }
+};
+
+/*!
+ * \brief Width of the gaussian function for the constraint damping
+ * parameter for the scalar driver equation.
+ */
+struct SigmaConstraintGamma2 {
+  static std::string name() {
+    return "SigmaConstraintGamma2(CurvedScalarWave)";
+  }
+  using type = double;
+  static constexpr Options::String help{
+      "Width of the gaussian function for the constraint damping parameter "
+      "for the scalar driver equation."};
+  static type lower_bound() { return 0.; }
+};
+
+/*!
+ * \brief Asymptotic value of the gaussian function for the constraint damping
+ * parameter for the scalar driver equation.
+ */
+struct OffsetConstraintGamma2 {
+  static std::string name() {
+    return "OffsetConstraintGamma2(CurvedScalarWave)";
+  }
+  using type = double;
+  static constexpr Options::String help{
+      "Asymptotic value for the gaussian function for the constraint damping "
+      "parameter for the scalar driver equation."};
+  static type lower_bound() { return 0.; }
+};
+
 }  // namespace OptionTags
 
 namespace Tags {
@@ -148,6 +194,33 @@ struct CswTwoIndexConstraintCompute
                                     const tnsr::ij<DataVector, Dim>&) =
       &CurvedScalarWave::two_index_constraint<Dim>;
   using base = Csw<CurvedScalarWave::Tags::TwoIndexConstraint<Dim>>;
+};
+
+struct AmplitudeConstraintGamma2 : db::SimpleTag {
+  using type = double;
+  using option_tags = tmpl::list<OptionTags::AmplitudeConstraintGamma2>;
+  static constexpr bool pass_metavariables = false;
+  static double create_from_options(const double amplitude_gaussian) {
+    return amplitude_gaussian;
+  }
+};
+
+struct SigmaConstraintGamma2 : db::SimpleTag {
+  using type = double;
+  using option_tags = tmpl::list<OptionTags::SigmaConstraintGamma2>;
+  static constexpr bool pass_metavariables = false;
+  static double create_from_options(const double sigma_gaussian) {
+    return sigma_gaussian;
+  }
+};
+
+struct OffsetConstraintGamma2 : db::SimpleTag {
+  using type = double;
+  using option_tags = tmpl::list<OptionTags::OffsetConstraintGamma2>;
+  static constexpr bool pass_metavariables = false;
+  static double create_from_options(const double offset_gaussian) {
+    return offset_gaussian;
+  }
 };
 
 }  // namespace Tags
