c22168b9847a3178a6bc21ce9105d44513339de5
Guillermo Lara <glara@aei.mpg.de>
2023-09-23 13:42:25 +0000

    Add ST sources

--- src/Evolution/Systems/ScalarTensor/Sources/ScalarSource.hpp
+++ src/Evolution/Systems/ScalarTensor/Sources/ScalarSource.hpp
@@ -6,8 +6,11 @@
 #include "DataStructures/DataBox/Tag.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "Evolution/Systems/ScalarTensor/Sources/Tags.hpp"
 #include "Evolution/Systems/ScalarTensor/Tags.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/WeylElectric.hpp"
+#include "PointwiseFunctions/GeneralRelativity/WeylMagnetic.hpp"
 #include "Utilities/Gsl.hpp"
 
 namespace ScalarTensor {
@@ -74,6 +77,33 @@ void add_scalar_source_to_dt_pi_scalar(
 void mass_source(gsl::not_null<Scalar<DataVector>*> scalar_source,
                  const Scalar<DataVector>& psi, const double mass_psi);
 
+void compute_scalar_curvature_source(
+    gsl::not_null<Scalar<DataVector>*> scalar_source,
+    const Scalar<DataVector>& weyl_electric_scalar,
+    const Scalar<DataVector>& weyl_magnetic_scalar,
+    const Scalar<DataVector>& psi, const double first_coupling_psi,
+    const double second_coupling_psi, const double mass_psi);
+
+Scalar<DataVector> compute_scalar_curvature_source(
+    const Scalar<DataVector>& weyl_electric_scalar,
+    const Scalar<DataVector>& weyl_magnetic_scalar,
+    const Scalar<DataVector>& psi, const double first_coupling_psi,
+    const double second_coupling_psi, const double mass_psi);
+
+void multiply_by_coupling_function_prime_quartic(
+    gsl::not_null<Scalar<DataVector>*> scalar_source,
+    const Scalar<DataVector>& psi, const double first_coupling_psi,
+    const double second_coupling_psi);
+
+// Extra functions for debugging
+void compute_coupling_function_derivative(
+    gsl::not_null<Scalar<DataVector>*> result, const Scalar<DataVector>& psi,
+    const double first_coupling_psi, const double second_coupling_psi);
+
+void compute_gb_scalar(gsl::not_null<Scalar<DataVector>*> gb_scalar,
+                       const Scalar<DataVector>& weyl_electric_scalar,
+                       const Scalar<DataVector>& weyl_magnetic_scalar);
+
 namespace Tags {
 
 /*!
@@ -90,6 +120,67 @@ struct ScalarSourceCompute : ScalarSource, db::ComputeTag {
                                     const Scalar<DataVector>&,
                                     const double) = &mass_source;
   using base = ScalarSource;
+};
+
+/*!
+ * \brief Compute tag for the scalar source given by the background curvature.
+ *
+ * \details Call compute_scalar_curvature_source. Needs that WeylElectric is in
+ * data box.
+ */
+template <size_t SpatialDim, typename Frame, typename DataType>
+struct ScalarCurvatureSourceCompute : ScalarSource, db::ComputeTag {
+  using argument_tags =
+      tmpl::list<gr::Tags::WeylElectricScalar<DataType>,
+                 gr::Tags::WeylMagneticScalar<DataType>,
+                 CurvedScalarWave::Tags::Psi,
+                 ScalarTensor::Sources::Tags::ScalarFirstCouplingParameter,
+                 ScalarTensor::Sources::Tags::ScalarSecondCouplingParameter,
+                 ScalarTensor::Tags::ScalarMass>;
+  using return_type = Scalar<DataVector>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result, const Scalar<DataVector>&,
+      const Scalar<DataVector>&, const Scalar<DataVector>&, const double,
+      const double, const double) = &compute_scalar_curvature_source;
+  using base = ScalarSource;
+};
+
+// Extra compute tags for debugging
+/*!
+ * \brief Compute tag for the coupling function.
+ *
+ * \details Call ....
+ */
+template <typename DataType>
+struct CouplingFunctionDerivativeCompute : CouplingFunctionDerivative,
+                                           db::ComputeTag {
+  //   using argument_tags = tmpl::list<fe::ScalarDriver::Psi>;
+  using argument_tags =
+      tmpl::list<CurvedScalarWave::Tags::Psi,
+                 ScalarTensor::Sources::Tags::ScalarFirstCouplingParameter,
+                 ScalarTensor::Sources::Tags::ScalarSecondCouplingParameter>;
+  using return_type = Scalar<DataVector>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result, const Scalar<DataVector>&,
+      const double, const double) = &compute_coupling_function_derivative;
+  using base = CouplingFunctionDerivative;
+};
+
+/*!
+ * \brief Compute tag for the GB scalar.
+ *
+ * \details Call compute_gb_scalar.
+ */
+template <typename DataType>
+struct GBScalarCompute : GBScalar, db::ComputeTag {
+  //   using argument_tags = tmpl::list<fe::ScalarDriver::Psi>;
+  using argument_tags = tmpl::list<gr::Tags::WeylElectricScalar<DataType>,
+                                   gr::Tags::WeylMagneticScalar<DataType>>;
+  using return_type = Scalar<DataVector>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result, const Scalar<DataVector>&,
+      const Scalar<DataVector>&) = &compute_gb_scalar;
+  using base = GBScalar;
 };
 
 }  // namespace Tags
