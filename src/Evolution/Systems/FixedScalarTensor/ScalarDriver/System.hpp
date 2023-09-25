ca8d61553780fba15f61f8744dfb9cfc314ffded
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 09:41:56 +0000

    Add scalar driver system

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/System.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/System.hpp
@@ -2,3 +2,50 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "DataStructures/VariablesTag.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/BoundaryCondition.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Characteristics.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/TimeDerivative.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/TMPL.hpp"
+
+/*!
+ * \ingroup EvolutionSystemsGroup
+ * \brief Items related to the scalar driver on a curved background
+ */
+namespace fe::ScalarDriver {
+struct System {
+  static constexpr bool is_in_flux_conservative_form = false;
+  static constexpr bool has_primitive_and_conservative_vars = false;
+  static constexpr size_t volume_dim = 3_st;
+  static constexpr bool is_euclidean = false;
+
+  using boundary_conditions_base = BoundaryConditions::BoundaryCondition;
+  using boundary_correction_base = BoundaryCorrections::BoundaryCorrection;
+
+  using variables_tag =
+      ::Tags::Variables<tmpl::list<Tags::Psi, Tags::Pi, Tags::Phi<3_st>>>;
+  using flux_variables = tmpl::list<>;
+  using gradient_variables = tmpl::list<Tags::Psi, Tags::Pi, Tags::Phi<3_st>>;
+
+  // Relic alias: needs to be removed once all evolution systems
+  // convert to using dg::ComputeTimeDerivative
+  using gradients_tags = gradient_variables;
+
+  using spacetime_tag_list = tmpl::list<>;
+
+  using compute_volume_time_derivative_terms = TimeDerivative;
+
+  using compute_largest_characteristic_speed =
+      Tags::ComputeLargestCharacteristicSpeed;
+
+  using inverse_spatial_metric_tag =
+      gr::Tags::InverseSpatialMetric<DataVector, 3_st>;
+};
+}  // namespace fe::ScalarDriver
