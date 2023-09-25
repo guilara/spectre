25499d8c5fcfae345ed0c426a459f2a7b0b6766c
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 15:45:10 +0000

    Add boundary corrections for fe system

--- src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/RegisterDerived.cpp
+++ src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/RegisterDerived.cpp
@@ -1,4 +1,13 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
+#include "Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/RegisterDerived.hpp"
+
+#include "Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/Factory.hpp"
+#include "Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp"
+
+namespace fe::DecoupledScalar::BoundaryCorrections {
+void register_derived_with_charm() {
+  register_derived_classes_with_charm<BoundaryCorrection>();
+}
+}  // namespace fe::DecoupledScalar::BoundaryCorrections
