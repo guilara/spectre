c93f45f0fbd11593b76f580cd5ad63b0d372d7b5
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 14:08:44 +0000

    Add gaussian constraint gamma for driver

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Actions/InitializeConstraintGammas.cpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Actions/InitializeConstraintGammas.cpp
@@ -1,4 +1,31 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Actions/InitializeConstraintGammas.hpp"
+
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace fe::ScalarDriver::Initialization {
+
+void InitializeConstraintDampingGammasGaussian::apply(
+    const gsl::not_null<Scalar<DataVector>*> gamma1,
+    const gsl::not_null<Scalar<DataVector>*> gamma2,
+    const tnsr::I<DataVector, 3, Frame::Inertial>& inertial_coords,
+    const double amplitude_gaussian, const double sigma_gaussian,
+    const double offset_gaussian) {
+  const size_t number_of_grid_points = get<0>(inertial_coords).size();
+  *gamma1 = Scalar<DataVector>{number_of_grid_points, 0.};
+  const double beta_gaussian = 1.0 / sigma_gaussian;
+  const auto radius = magnitude(inertial_coords);
+  get(*gamma2) =
+      amplitude_gaussian * exp(-square(beta_gaussian * radius.get())) +
+      offset_gaussian;
+}
+
+}  // namespace fe::ScalarDriver::Initialization
--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Actions/InitializeConstraintGammas.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Actions/InitializeConstraintGammas.hpp
@@ -2,3 +2,59 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/Protocols/Mutator.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Domain/Tags.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "Utilities/ProtocolHelpers.hpp"
+
+namespace fe::ScalarDriver::Initialization {
+
+/*!
+ * \ingroup InitializationGroup
+ * \brief Initializes the constraint damping parameters \f$\gamma_1\f$ and
+ * \f$\gamma_2\f$.
+ *
+ * \details The constraint damping parameters  of the Driver system
+ * are initialized according to a Gaussian
+ * \f{align*}
+ *  \gamma_1 &= 0 \\
+ *  \gamma_2 &= A e^{-(\sigma r)^2 } + c,
+ * \f}
+ * where \f$r = \sqrt{\delta_{ij} x^i x^j}\f$ and with \f$A = 10\f$, \f$\sigma =
+ * 10^{-1}\f$, \f$c = 10^{-4}\f$. These values were experimentally found to
+ * ensure a stable evolution when using the worldtube scheme.
+ *
+ *  DataBox changes:
+ * - Adds:
+ *   * `fe::ScalarDriver::Tags::ConstraintGamma1`
+ *   * `fe::ScalarDriver::Tags::ConstraintGamma2`
+ * - Removes: nothing
+ * - Modifies: nothing
+ */
+struct InitializeConstraintDampingGammasGaussian
+    : tt::ConformsTo<db::protocols::Mutator> {
+  using return_tags = tmpl::list<fe::ScalarDriver::Tags::ConstraintGamma1,
+                                 fe::ScalarDriver::Tags::ConstraintGamma2>;
+  using argument_tags =
+      tmpl::list<domain::Tags::Coordinates<3, Frame::Inertial>,
+                 fe::ScalarDriver::Tags::AmplitudeConstraintGamma2,
+                 fe::ScalarDriver::Tags::SigmaConstraintGamma2,
+                 fe::ScalarDriver::Tags::OffsetConstraintGamma2>;
+  using simple_tags = return_tags;
+  using compute_tags = tmpl::list<>;
+  using simple_tags_from_options = tmpl::list<>;
+  using const_global_cache_tags = tmpl::list<>;
+  using mutable_global_cache_tags = tmpl::list<>;
+  static void apply(
+      const gsl::not_null<Scalar<DataVector>*> gamma1,
+      const gsl::not_null<Scalar<DataVector>*> gamma2,
+      const tnsr::I<DataVector, 3, Frame::Inertial>& inertial_coords,
+      const double amplitude_gaussian, const double sigma_gaussian,
+      const double offset_gaussian);
+};
+}  // namespace fe::ScalarDriver::Initialization
