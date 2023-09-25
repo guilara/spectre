018d50b3f6c80d3ef05733a48bc242caabedecbc
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 14:06:13 +0000

    Add driver boundary conditions

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/BoundaryCondition.cpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/BoundaryCondition.cpp
@@ -1,4 +1,19 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/BoundaryCondition.hpp"
+
+#include <pup.h>
+
+#include "Domain/BoundaryConditions/BoundaryCondition.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+
+namespace fe::ScalarDriver::BoundaryConditions {
+BoundaryCondition::BoundaryCondition(CkMigrateMessage* const msg)
+    : domain::BoundaryConditions::BoundaryCondition(msg) {}
+
+void BoundaryCondition::pup(PUP::er& p) {
+  domain::BoundaryConditions::BoundaryCondition::pup(p);
+}
+
+}  // namespace fe::ScalarDriver::BoundaryConditions
--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
@@ -2,3 +2,18 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "Domain/BoundaryConditions/Periodic.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/AnalyticConstant.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/DemandOutgoingCharSpeeds.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace fe::ScalarDriver::BoundaryConditions {
+/// Typelist of standard BoundaryConditions
+using standard_boundary_conditions =
+    tmpl::list<DemandOutgoingCharSpeeds,
+               AnalyticConstant,
+               domain::BoundaryConditions::Periodic<BoundaryCondition>>;
+}  // namespace fe::ScalarDriver::BoundaryConditions
