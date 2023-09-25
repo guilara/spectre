95425cd061f0e31d302e2d421ebb44b0d6983362
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 15:45:10 +0000

    Add boundary corrections for fe system

--- src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/Factory.hpp
+++ src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/Factory.hpp
@@ -2,3 +2,6 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include "Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/BoundaryCorrection.hpp"
+#include "Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/BoundaryCorrections/ProductOfCorrections.hpp"
