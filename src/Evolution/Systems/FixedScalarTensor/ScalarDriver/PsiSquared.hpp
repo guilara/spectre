17054aee2a8bdb3e2201455d40c376a891daa31a
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 10:07:27 +0000

    Add psi squared for driver

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/PsiSquared.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/PsiSquared.hpp
@@ -2,3 +2,35 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+#include <string>
+
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace fe::ScalarDriver::Tags {
+/*!
+ * \brief The square of the scalar field \f$\Psi\f$.
+ */
+struct PsiSquared : db::SimpleTag {
+  using type = Scalar<DataVector>;
+  static std::string name() { return "PsiSquared(ScalarDriver)"; }
+};
+
+/*!
+ * \brief Compute tag that calculates the square of the scalar field \f$\Psi\f$.
+ */
+struct PsiSquaredCompute : PsiSquared, db::ComputeTag {
+  using base = PsiSquared;
+  using return_type = Scalar<DataVector>;
+  using argument_tags = tmpl::list<fe::ScalarDriver::Tags::Psi>;
+  static void function(const gsl::not_null<Scalar<DataVector>*> psi_squared,
+                       const Scalar<DataVector>& psi) {
+    get(*psi_squared) = get(psi) * get(psi);
+  }
+};
+}  // namespace fe::ScalarDriver::Tags
