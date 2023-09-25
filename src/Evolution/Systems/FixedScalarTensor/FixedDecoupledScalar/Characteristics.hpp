d1cf1e2f095acb6e788644b5c3ec7411864fd818
Guillermo Lara <glara@aei.mpg.de>
2023-09-25 14:26:42 +0000

    Add largest char speed for fe system

--- src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/Characteristics.hpp
+++ src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/Characteristics.hpp
@@ -2,3 +2,61 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace fe::DecoupledScalar::Tags {
+struct LargestCharacteristicSpeed : db::SimpleTag {
+  using type = double;
+};
+
+/*!
+ * \brief Computes the largest magnitude of the characteristic speeds.
+ *
+ * \warning Assumes \f$-1\le\gamma_1\le0\f$.
+ */
+template <typename Frame = Frame::Inertial>
+struct ComputeLargestCharacteristicSpeed : db::ComputeTag,
+                                           LargestCharacteristicSpeed {
+  using argument_tags =
+      tmpl::list<::gh::ConstraintDamping::Tags::ConstraintGamma1,
+                 gr::Tags::Lapse<DataVector>,
+                 gr::Tags::Shift<DataVector, 3, Frame>,
+                 gr::Tags::SpatialMetric<DataVector, 3, Frame>,
+                 CurvedScalarWave::Tags::ConstraintGamma1,
+                 fe::ScalarDriver::Tags::ConstraintGamma1>;
+  using return_type = double;
+  using base = LargestCharacteristicSpeed;
+  static void function(const gsl::not_null<double*> speed,
+                       // GH arguments
+                       const Scalar<DataVector>& gamma_1,
+                       const Scalar<DataVector>& lapse,
+                       const tnsr::I<DataVector, 3, Frame>& shift,
+                       const tnsr::ii<DataVector, 3, Frame>& spatial_metric,
+                       // Scalar arguments
+                       const Scalar<DataVector>& gamma_1_scalar,
+                       // Driver arguments
+                       const Scalar<DataVector>& gamma_1_driver) {
+    // Largest speed in for ScalarTensor
+    double st_largest_speed = 0.0;
+    ScalarTensor::Tags::ComputeLargestCharacteristicSpeed<Frame>::function(
+        make_not_null(&st_largest_speed), gamma_1, lapse, shift, spatial_metric,
+        gamma_1_scalar);
+    // Largest speed for ScalarDriver
+    double driver_largest_speed = 0.0;
+    fe::ScalarDriver::Tags::ComputeLargestCharacteristicSpeed::function(
+        make_not_null(&driver_largest_speed), gamma_1_driver, lapse, shift,
+        spatial_metric);
+    // Compute the maximum speed
+    *speed = std::max(st_largest_speed, driver_largest_speed);
+  }
+};
+}  // namespace fe::DecoupledScalar::Tags
