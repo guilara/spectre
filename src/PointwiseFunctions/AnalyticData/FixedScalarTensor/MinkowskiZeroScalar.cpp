335715db42948ee962c4d3112071f3601e3c2158
Guillermo Lara <glara@aei.mpg.de>
2023-09-26 08:47:53 +0000

    Fix

--- src/PointwiseFunctions/AnalyticData/FixedScalarTensor/MinkowskiZeroScalar.cpp
+++ src/PointwiseFunctions/AnalyticData/FixedScalarTensor/MinkowskiZeroScalar.cpp
@@ -1,4 +1,129 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
+#include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
+
+#include <cmath>
+#include <cstddef>
+
+#include "DataStructures/DataVector.hpp"     // IWYU pragma: keep
+#include "DataStructures/Tensor/Tensor.hpp"  // IWYU pragma: keep
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/ErrorHandling/Assert.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeWithValue.hpp"
+
+// IWYU pragma:  no_include "DataStructures/Tensor/TypeAliases.hpp"
+
+namespace fe::DecoupledScalar::AnalyticData {
+
+MinkowskiZeroScalar::MinkowskiZeroScalar(double amplitude,
+                                         double amplitude_driver) {
+  amplitude_ = amplitude;
+  amplitude_driver_ = amplitude_driver;
+}
+
+std::unique_ptr<evolution::initial_data::InitialData>
+MinkowskiZeroScalar::get_clone() const {
+  return std::make_unique<MinkowskiZeroScalar>(*this);
+}
+
+MinkowskiZeroScalar::MinkowskiZeroScalar(CkMigrateMessage* msg)
+    : InitialData(msg) {}
+
+void MinkowskiZeroScalar::pup(PUP::er& p) {
+  InitialData::pup(p);
+  p | amplitude_;
+  p | amplitude_driver_;
+  p | background_spacetime_;
+}
+
+template <typename DataType>
+tuples::TaggedTuple<CurvedScalarWave::Tags::Psi> MinkowskiZeroScalar::variables(
+    const tnsr::I<DataType, 3>& x,
+    tmpl::list<CurvedScalarWave::Tags::Psi> /*meta*/) const {
+  return {make_with_value<Scalar<DataType>>(x, amplitude_)};
+}
+
+template <typename DataType>
+tuples::TaggedTuple<CurvedScalarWave::Tags::Phi<3_st>>
+MinkowskiZeroScalar::variables(
+    const tnsr::I<DataType, 3>& x,
+    tmpl::list<CurvedScalarWave::Tags::Phi<3_st>> /*meta*/) const {
+  return {make_with_value<tnsr::i<DataType, 3>>(x, 0.0)};
+}
+
+template <typename DataType>
+tuples::TaggedTuple<CurvedScalarWave::Tags::Pi> MinkowskiZeroScalar::variables(
+    const tnsr::I<DataType, 3>& x,
+    tmpl::list<CurvedScalarWave::Tags::Pi> /*meta*/) const {
+  return {make_with_value<Scalar<DataType>>(x, 0.0)};
+}
+
+//
+
+template <typename DataType>
+tuples::TaggedTuple<fe::ScalarDriver::Tags::Psi> MinkowskiZeroScalar::variables(
+    const tnsr::I<DataType, 3>& x,
+    tmpl::list<fe::ScalarDriver::Tags::Psi> /*meta*/) const {
+  return {make_with_value<Scalar<DataType>>(x, amplitude_driver_)};
+}
+
+template <typename DataType>
+tuples::TaggedTuple<fe::ScalarDriver::Tags::Phi<3_st>>
+MinkowskiZeroScalar::variables(
+    const tnsr::I<DataType, 3>& x,
+    tmpl::list<fe::ScalarDriver::Tags::Phi<3_st>> /*meta*/) const {
+  return {make_with_value<tnsr::i<DataType, 3>>(x, 0.0)};
+}
+
+template <typename DataType>
+tuples::TaggedTuple<fe::ScalarDriver::Tags::Pi> MinkowskiZeroScalar::variables(
+    const tnsr::I<DataType, 3>& x,
+    tmpl::list<fe::ScalarDriver::Tags::Pi> /*meta*/) const {
+  return {make_with_value<Scalar<DataType>>(x, 0.0)};
+}
+
+PUP::able::PUP_ID MinkowskiZeroScalar::my_PUP_ID = 0;
+
+bool operator==(const MinkowskiZeroScalar& lhs,
+                const MinkowskiZeroScalar& rhs) {
+  return lhs.background_spacetime_ == rhs.background_spacetime_ and
+         lhs.amplitude_ == rhs.amplitude_ and
+         lhs.amplitude_driver_ == rhs.amplitude_driver_;
+}
+
+bool operator!=(const MinkowskiZeroScalar& lhs,
+                const MinkowskiZeroScalar& rhs) {
+  return not(lhs == rhs);
+}
+
+#define DTYPE(data) BOOST_PP_TUPLE_ELEM(0, data)
+#define TAG(data) BOOST_PP_TUPLE_ELEM(1, data)
+
+#define INSTANTIATE_SCALARS(_, data)                                      \
+  template tuples::TaggedTuple<TAG(data)> MinkowskiZeroScalar::variables( \
+      const tnsr::I<DTYPE(data), 3>& x, tmpl::list<TAG(data)> /*meta*/) const;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE_SCALARS, (DataVector),
+                        (CurvedScalarWave::Tags::Psi,
+                         CurvedScalarWave::Tags::Pi,
+                         fe::ScalarDriver::Tags::Psi,
+                         fe::ScalarDriver::Tags::Pi))
+
+#define INSTANTIATE_VECTORS(_, data)                                      \
+  template tuples::TaggedTuple<TAG(data)> MinkowskiZeroScalar::variables( \
+      const tnsr::I<DTYPE(data), 3>& x, tmpl::list<TAG(data)> /*meta*/) const;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE_VECTORS, (DataVector),
+                        (CurvedScalarWave::Tags::Phi<3_st>,
+                         fe::ScalarDriver::Tags::Phi<3_st>))
+
+#undef DTYPE
+#undef TAG
+#undef INSTANTIATE_SCALARS
+#undef INSTANTIATE_VECTORS
+}  // namespace fe::DecoupledScalar::AnalyticData
--- src/PointwiseFunctions/AnalyticData/FixedScalarTensor/MinkowskiZeroScalar.hpp
+++ src/PointwiseFunctions/AnalyticData/FixedScalarTensor/MinkowskiZeroScalar.hpp
@@ -2,3 +2,152 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+#include <array>
+#include <limits>
+#include <string>
+
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
+#include "Options/String.hpp"
+#include "PointwiseFunctions/AnalyticData/AnalyticData.hpp"
+#include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/AnalyticData.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/Minkowski.hpp"
+#include "PointwiseFunctions/InitialDataUtilities/InitialData.hpp"
+#include "Utilities/Serialization/CharmPupable.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+// IWYU pragma:  no_include <pup.h>
+
+/// \cond
+namespace PUP {
+class er;  // IWYU pragma: keep
+}  // namespace PUP
+/// \endcond
+
+namespace fe::DecoupledScalar::AnalyticData {
+/*!
+ * \brief Set the scalar variables to zero on Minkowski space.
+ */
+class MinkowskiZeroScalar : public virtual evolution::initial_data::InitialData,
+                            public MarkAsAnalyticData,
+                            public AnalyticDataBase {
+ public:
+  /// The amplitude of the scalar field
+  struct Amplitude {
+    using type = double;
+    static constexpr Options::String help = {
+        "The amplitude of the scalar field."};
+  };
+
+  /// The amplitude of the scalar field
+  struct AmplitudeDriver {
+    using type = double;
+    static constexpr Options::String help = {
+        "The amplitude of the driver field."};
+  };
+
+  using options = tmpl::list<Amplitude, AmplitudeDriver>;
+  static constexpr Options::String help = {
+      "Zero scalar field in Minkowski space."};
+
+  MinkowskiZeroScalar() = default;
+  MinkowskiZeroScalar(const MinkowskiZeroScalar& /*rhs*/) = default;
+  MinkowskiZeroScalar& operator=(const MinkowskiZeroScalar& /*rhs*/) = default;
+  MinkowskiZeroScalar(MinkowskiZeroScalar&& /*rhs*/) = default;
+  MinkowskiZeroScalar& operator=(MinkowskiZeroScalar&& /*rhs*/) = default;
+  ~MinkowskiZeroScalar() override = default;
+
+  MinkowskiZeroScalar(double amplitude, double amplitude_driver);
+
+  auto get_clone() const
+      -> std::unique_ptr<evolution::initial_data::InitialData> override;
+
+  /// \cond
+  explicit MinkowskiZeroScalar(CkMigrateMessage* msg);
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(MinkowskiZeroScalar);
+  /// \endcond
+
+  // The extra tags below can also be added as common to all scalar tensor
+  // solutions
+  template <typename DataType, typename Frame = Frame::Inertial>
+  using tags = tmpl::flatten<tmpl::list<
+      typename AnalyticDataBase::template tags<DataType>,
+      gr::Tags::DerivDetSpatialMetric<DataType, 3_st, Frame>,
+      gr::Tags::TraceExtrinsicCurvature<DataType>,
+      gr::Tags::SpatialChristoffelFirstKind<DataType, 3_st, Frame>,
+      gr::Tags::SpatialChristoffelSecondKind<DataType, 3_st, Frame>,
+      gr::Tags::TraceSpatialChristoffelSecondKind<DataType, 3_st, Frame>>>;
+
+  /// @{
+  /// Retrieve scalar variable at `(x, t)`
+  template <typename DataType>
+  auto variables(const tnsr::I<DataType, 3>& x,
+                 tmpl::list<CurvedScalarWave::Tags::Psi> /*meta*/) const
+      -> tuples::TaggedTuple<CurvedScalarWave::Tags::Psi>;
+
+  template <typename DataType>
+  auto variables(const tnsr::I<DataType, 3>& x,
+                 tmpl::list<CurvedScalarWave::Tags::Phi<3_st>> /*meta*/) const
+      -> tuples::TaggedTuple<CurvedScalarWave::Tags::Phi<3_st>>;
+
+  template <typename DataType>
+  auto variables(const tnsr::I<DataType, 3>& x,
+                 tmpl::list<CurvedScalarWave::Tags::Pi> /*meta*/) const
+      -> tuples::TaggedTuple<CurvedScalarWave::Tags::Pi>;
+  /// @}
+
+  /// @{
+  /// Retrieve scalar driver variable at `x`
+  template <typename DataType>
+  auto variables(const tnsr::I<DataType, 3>& x,
+                 tmpl::list<fe::ScalarDriver::Tags::Psi> /*meta*/) const
+      -> tuples::TaggedTuple<fe::ScalarDriver::Tags::Psi>;
+
+  template <typename DataType>
+  auto variables(const tnsr::I<DataType, 3>& x,
+                 tmpl::list<fe::ScalarDriver::Tags::Phi<3_st>> /*meta*/) const
+      -> tuples::TaggedTuple<fe::ScalarDriver::Tags::Phi<3_st>>;
+
+  template <typename DataType>
+  auto variables(const tnsr::I<DataType, 3>& x,
+                 tmpl::list<fe::ScalarDriver::Tags::Pi> /*meta*/) const
+      -> tuples::TaggedTuple<fe::ScalarDriver::Tags::Pi>;
+  /// @}
+
+  /// Retrieve a collection of scalar variables at `x`
+  template <typename DataType, typename... Tags>
+  tuples::TaggedTuple<Tags...> variables(const tnsr::I<DataType, 3>& x,
+                                         tmpl::list<Tags...> /*meta*/) const {
+    static_assert(sizeof...(Tags) > 1,
+                  "The generic template will recurse infinitely if only one "
+                  "tag is being retrieved.");
+    return {get<Tags>(variables(x, tmpl::list<Tags>{}))...};
+  }
+
+  /// Retrieve the metric variables
+  template <typename DataType, typename Tag>
+  tuples::TaggedTuple<Tag> variables(const tnsr::I<DataType, 3>& x,
+                                     tmpl::list<Tag> /*meta*/) const {
+    // We need to provide a time argument for the background solution
+    return background_spacetime_.variables(x, 0.0, tmpl::list<Tag>{});
+  }
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& /*p*/) override;
+
+ protected:
+  friend bool operator==(const MinkowskiZeroScalar& lhs,
+                         const MinkowskiZeroScalar& rhs);
+
+  double amplitude_ = std::numeric_limits<double>::signaling_NaN();
+  double amplitude_driver_ = std::numeric_limits<double>::signaling_NaN();
+  gr::Solutions::Minkowski<3> background_spacetime_{};
+};
+
+bool operator!=(const MinkowskiZeroScalar& lhs, const MinkowskiZeroScalar& rhs);
+
+}  // namespace fe::DecoupledScalar::AnalyticData
