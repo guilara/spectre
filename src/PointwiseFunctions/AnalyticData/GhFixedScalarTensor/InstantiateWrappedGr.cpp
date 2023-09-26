40d7266519be4149e5d980c55abe40404b518082
Guillermo Lara <glara@aei.mpg.de>
2023-09-26 12:20:45 +0000

    Wrapped gr

--- src/PointwiseFunctions/AnalyticData/GhFixedScalarTensor/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticData/GhFixedScalarTensor/InstantiateWrappedGr.cpp
@@ -1,4 +1,16 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
+#include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/SphericalKerrSchildSH.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
+#include "Utilities/GenerateInstantiations.hpp"
+
+GENERATE_INSTANTIATIONS(
+    WRAPPED_GR_INSTANTIATE,
+    (fe::DecoupledScalar::AnalyticData::MinkowskiZeroScalar,
+     fe::DecoupledScalar::AnalyticData::KerrSphericalHarmonic,
+     fe::DecoupledScalar::AnalyticData::SphericalKerrSchildSH))
