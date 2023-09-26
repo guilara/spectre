e3795db09a062e6aa738757c1dcf59c3f59f3102
Guillermo Lara <glara@aei.mpg.de>
2023-09-26 08:34:09 +0000

    Update wrapped gr factory for st id

--- src/PointwiseFunctions/AnalyticData/GhScalarTensor/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticData/GhScalarTensor/InstantiateWrappedGr.cpp
@@ -3,9 +3,13 @@
 
 #include "DataStructures/DataBox/Prefixes.hpp"
 #include "PointwiseFunctions/AnalyticData/ScalarTensor/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticData/ScalarTensor/MinkowskiZeroScalar.hpp"
+#include "PointwiseFunctions/AnalyticData/ScalarTensor/SphericalKerrSchildSH.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
 #include "Utilities/GenerateInstantiations.hpp"
 
 GENERATE_INSTANTIATIONS(WRAPPED_GR_INSTANTIATE,
-                        (ScalarTensor::AnalyticData::KerrSphericalHarmonic))
+                        (ScalarTensor::AnalyticData::MinkowskiZeroScalar,
+                         ScalarTensor::AnalyticData::KerrSphericalHarmonic,
+                         ScalarTensor::AnalyticData::SphericalKerrSchildSH))
