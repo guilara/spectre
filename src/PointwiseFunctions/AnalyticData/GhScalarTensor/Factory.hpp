36a1f72b27cb58952a6ca6e0bb9133e26f44de19
Guillermo Lara <glara@aei.mpg.de>
2023-09-26 08:34:09 +0000

    Update wrapped gr factory for st id

--- src/PointwiseFunctions/AnalyticData/GhScalarTensor/Factory.hpp
+++ src/PointwiseFunctions/AnalyticData/GhScalarTensor/Factory.hpp
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "PointwiseFunctions/AnalyticData/ScalarTensor/KerrSphericalHarmonic.hpp"
+#include "PointwiseFunctions/AnalyticData/ScalarTensor/MinkowskiZeroScalar.hpp"
+#include "PointwiseFunctions/AnalyticData/ScalarTensor/SphericalKerrSchildSH.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -11,7 +13,11 @@
 namespace gh::ScalarTensor::AnalyticData {
 
 /// \brief List of all analytic solutions
-using all_analytic_data = tmpl::list<gh::Solutions::WrappedGr<
-    ::ScalarTensor::AnalyticData::KerrSphericalHarmonic>>;
+using all_analytic_data = tmpl::list<
+    gh::Solutions::WrappedGr<::ScalarTensor::AnalyticData::MinkowskiZeroScalar>,
+    gh::Solutions::WrappedGr<
+        ::ScalarTensor::AnalyticData::KerrSphericalHarmonic>,
+    gh::Solutions::WrappedGr<
+        ::ScalarTensor::AnalyticData::SphericalKerrSchildSH>>;
 
 }  // namespace gh::ScalarTensor::AnalyticData
