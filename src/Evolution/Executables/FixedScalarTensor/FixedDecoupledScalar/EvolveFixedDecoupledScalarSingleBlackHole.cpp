8f648729c19884eeb0ba921bc72aa503587938e4
Guillermo Lara <glara@aei.mpg.de>
2023-10-09 15:49:52 +0000

    Fix

--- src/Evolution/Executables/FixedScalarTensor/FixedDecoupledScalar/EvolveFixedDecoupledScalarSingleBlackHole.cpp
+++ src/Evolution/Executables/FixedScalarTensor/FixedDecoupledScalar/EvolveFixedDecoupledScalarSingleBlackHole.cpp
@@ -1,7 +1,7 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#include "Evolution/Executables/FixedScalarTensor/FixedDecoupledScalar/EvolveScalarTensorSingleBlackHole.hpp"
+#include "Evolution/Executables/FixedScalarTensor/FixedDecoupledScalar/EvolveFixedDecoupledScalarSingleBlackHole.hpp"
 
 #include <vector>
 
@@ -17,13 +17,13 @@
 using metavariables = EvolutionMetavars<DIM, USE_NUMERICAL_ID>;
 
 extern "C" void CkRegisterMainModule() {
-  Parallel::charmxx::register_main_module<EvolutionMetavars>();
+  Parallel::charmxx::register_main_module<metavariables>();
   Parallel::charmxx::register_init_node_and_proc(
       {&domain::creators::register_derived_with_charm,
        &domain::creators::time_dependence::register_derived_with_charm,
        &domain::FunctionsOfTime::register_derived_with_charm,
        &fe::DecoupledScalar::BoundaryCorrections::register_derived_with_charm,
        &gh::ConstraintDamping::register_derived_with_charm,
-       &register_factory_classes_with_charm<EvolutionMetavars>},
+       &register_factory_classes_with_charm<metavariables>},
       {});
 }
