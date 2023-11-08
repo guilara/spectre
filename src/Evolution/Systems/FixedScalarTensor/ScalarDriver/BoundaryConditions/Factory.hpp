3e427ecf83e02e686d421cfaabccbc0ba804fa94
Guillermo Lara <glara@aei.mpg.de>
2023-11-08 15:37:25 +0000

    Fix

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/Factory.hpp
@@ -7,13 +7,14 @@
 
 #include "Domain/BoundaryConditions/Periodic.hpp"
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/AnalyticConstant.hpp"
+#include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/ConstraintPreservingSphericalRadiation.hpp"
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/BoundaryConditions/DemandOutgoingCharSpeeds.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace fe::ScalarDriver::BoundaryConditions {
 /// Typelist of standard BoundaryConditions
 using standard_boundary_conditions =
-    tmpl::list<DemandOutgoingCharSpeeds,
-               AnalyticConstant,
+    tmpl::list<AnalyticConstant, DemandOutgoingCharSpeeds,
+               ConstraintPreservingSphericalRadiation,
                domain::BoundaryConditions::Periodic<BoundaryCondition>>;
 }  // namespace fe::ScalarDriver::BoundaryConditions
