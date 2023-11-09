5b840543cc2b49f986e90a11a652c21589f798a8
Guillermo Lara <glara@aei.mpg.de>
2023-11-09 15:21:22 +0000

    Add details and add to executable

--- src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/ConstraintDamping/RegisterDerivedWithCharm.hpp
+++ src/Evolution/Systems/FixedScalarTensor/FixedDecoupledScalar/ConstraintDamping/RegisterDerivedWithCharm.hpp
@@ -2,3 +2,7 @@
 // See LICENSE.txt for details.
 
 #pragma once
+
+namespace fe::DecoupledScalar::ConstraintDamping {
+void register_derived_with_charm();
+}  // namespace fe::DecoupledScalar::ConstraintDamping
