6aee30aa7988ad07f7a7ec8360fb0712b09a14b8
Guillermo Lara <glara@aei.mpg.de>
2023-11-10 10:29:10 +0000

    Fix typos

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Diagnostics.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Diagnostics.hpp
@@ -17,8 +17,8 @@ namespace fe::ScalarDriver {
 void driver_tracking_diagnostic(
     const gsl::not_null<Scalar<DataVector>*> diagnostic,
     const Scalar<DataVector>& psi, const Scalar<DataVector>& target_psi,
-    const Scalar<DataType>& scalar_tau_parameter,
-    const Scalar<DataType>& scalar_sigma_parameter);
+    const Scalar<DataVector>& scalar_tau_parameter,
+    const Scalar<DataVector>& scalar_sigma_parameter);
 
 namespace Tags {
 
@@ -33,13 +33,11 @@ struct TrackingDiagnosticCompute : TrackingDiagnostic, db::ComputeTag {
       tmpl::list<fe::ScalarDriver::Tags::Psi, fe::ScalarDriver::Tags::TargetPsi,
                  fe::ScalarDriver::Tags::ScalarTauParameter,
                  fe::ScalarDriver::Tags::ScalarSigmaParameter>;
-  using return_type = Scalar<DataVector>;
-  static constexpr void (*function)(const gsl::not_null<return_type*> result,
-                                    const Scalar<DataVector>&,
-                                    const Scalar<DataVector>&,
-                                    const Scalar<DataVector>&,
-                                    const Scalar<DataVector>&) =
-      &fe::ScalarDriver::driver_tracking_diagnostic;
+  using return_type = Scalar<DataType>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result, const Scalar<DataType>&,
+      const Scalar<DataType>&, const Scalar<DataType>&,
+      const Scalar<DataType>&) = &fe::ScalarDriver::driver_tracking_diagnostic;
   using base = TrackingDiagnostic;
 };
 
