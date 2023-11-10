af657d5d9d08945e5daa8227be9ef1134d69492d
Guillermo Lara <glara@aei.mpg.de>
2023-11-10 21:45:34 +0000

    Observe mesh velocity

--- src/Evolution/Executables/FixedScalarTensor/FixedDecoupledScalar/FixedScalarTensorBase.hpp
+++ src/Evolution/Executables/FixedScalarTensor/FixedDecoupledScalar/FixedScalarTensorBase.hpp
@@ -403,7 +403,8 @@ struct ObserverTags {
               fe::ScalarDriver::Tags::TwoIndexConstraint>,
           // Coordinates
           ::domain::Tags::Coordinates<volume_dim, Frame::Grid>,
-          ::domain::Tags::Coordinates<volume_dim, Frame::Inertial>>,
+          ::domain::Tags::Coordinates<volume_dim, Frame::Inertial>,
+          ::Events::Tags::ObserverMeshVelocity<volume_dim, Frame::Inertial>>,
       //   error_tags,
       // The 4-index constraint is only implemented in 3d
       tmpl::conditional_t<
