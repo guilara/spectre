a8ea00ba20a3e6dafb680c97da426cd91c2871b4
Guillermo Lara <glara@aei.mpg.de>
2023-11-13 16:22:46 +0000

    Observe shift minus mesh velocity

--- src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Diagnostics.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarDriver/Diagnostics.hpp
@@ -4,12 +4,15 @@
 #pragma once
 
 #include "DataStructures/DataBox/Tag.hpp"
+#include "Domain/Tags.hpp"
+#include "Domain/TagsTimeDependent.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Tags.hpp"
 #include "Evolution/Systems/FixedScalarTensor/ScalarDriver/Tags.hpp"
 #include "Evolution/Systems/ScalarTensor/Sources/ScalarSource.hpp"
 #include "Evolution/Systems/ScalarTensor/Sources/Tags.hpp"
 #include "Evolution/Systems/ScalarTensor/Tags.hpp"
 #include "Options/String.hpp"
+#include "ParallelAlgorithms/Events/Tags.hpp"
 #include "Utilities/Gsl.hpp"
 
 namespace fe::ScalarDriver {
@@ -19,6 +22,11 @@ void driver_tracking_diagnostic(
     const Scalar<DataVector>& psi, const Scalar<DataVector>& target_psi,
     const Scalar<DataVector>& scalar_tau_parameter,
     const Scalar<DataVector>& scalar_sigma_parameter);
+
+void shift_minus_mesh_velocity(
+    const gsl::not_null<tnsr::I<DataVector, 3>*> result,
+    const tnsr::I<DataVector, 3>& shift,
+    const std::optional<tnsr::I<DataVector, 3>>& mesh_velocity);
 
 namespace Tags {
 
@@ -39,6 +47,21 @@ struct TrackingDiagnosticCompute : TrackingDiagnostic, db::ComputeTag {
       const Scalar<DataType>&, const Scalar<DataType>&,
       const Scalar<DataType>&) = &fe::ScalarDriver::driver_tracking_diagnostic;
   using base = TrackingDiagnostic;
+};
+
+/*!
+ * \brief Compute the difference between the shift and mesh velocity.
+ */
+struct ShiftMinusMeshVelocityCompute : ShiftMinusMeshVelocity, db::ComputeTag {
+  using argument_tags =
+      tmpl::list<gr::Tags::Shift<DataVector, 3_st>,
+                 Events::Tags::ObserverMeshVelocity<3_st, Frame::Inertial>>;
+  using return_type = tnsr::I<DataVector, 3>;
+  static constexpr void (*function)(
+      const gsl::not_null<return_type*> result, const tnsr::I<DataVector, 3>&,
+      const std::optional<tnsr::I<DataVector, 3>>&) =
+      &fe::ScalarDriver::shift_minus_mesh_velocity;
+  using base = ShiftMinusMeshVelocity;
 };
 
 }  // namespace Tags
