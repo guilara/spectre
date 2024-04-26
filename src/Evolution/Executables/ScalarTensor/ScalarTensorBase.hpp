29467aa1cacd3370f83e104d63ff4ffb46546097
Guillermo Lara <glara@aei.mpg.de>
2024-04-26 16:16:51 +0000

    Add rhs compute tags

--- src/Evolution/Executables/ScalarTensor/ScalarTensorBase.hpp
+++ src/Evolution/Executables/ScalarTensor/ScalarTensorBase.hpp
@@ -268,6 +268,8 @@ struct ObserverTags {
           ScalarTensor::Tags::CouplingFunctionDerivativeCompute<DataVector>,
           // BR
           ScalarTensor::Tags::OrderReducedGBScalarCompute<Frame::Inertial>,
+          ScalarTensor::Tags::RhsPsiCompute, ScalarTensor::Tags::RhsPiCompute,
+          ScalarTensor::Tags::RhsPhiCompute,
 
           ::domain::Tags::Coordinates<volume_dim, Frame::Grid>,
           ::domain::Tags::Coordinates<volume_dim, Frame::Inertial>>,
