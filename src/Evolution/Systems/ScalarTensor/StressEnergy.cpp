b8fa3dff7d789bebac1f73d6b03ce5ff93dd5798
Guillermo Lara <glara@aei.mpg.de>
2024-05-03 14:19:40 +0000

    Move kappa inside stress-energy tensor

--- src/Evolution/Systems/ScalarTensor/StressEnergy.cpp
+++ src/Evolution/Systems/ScalarTensor/StressEnergy.cpp
@@ -15,10 +15,11 @@ void add_stress_energy_term_to_dt_pi(
     const gsl::not_null<tnsr::aa<DataVector, 3_st>*> dt_pi,
     const tnsr::aa<DataVector, 3_st>& trace_reversed_stress_energy,
     const Scalar<DataVector>& lapse) {
+  // We move the M_PI factor to the trace reversed computation
   for (size_t a = 0; a < 4; ++a) {
     for (size_t b = a; b < 4; ++b) {
       dt_pi->get(a, b) -= 0.0 *  // We turn off the backreaction
-                          16.0 * M_PI * get(lapse) *
+                          2.0 * get(lapse) *
                           trace_reversed_stress_energy.get(a, b);
     }
   }
@@ -29,14 +30,16 @@ void trace_reversed_stress_energy(
     const Scalar<DataVector>& pi_scalar,
     const tnsr::i<DataVector, 3_st>& phi_scalar,
     const Scalar<DataVector>& lapse) {
-  get<0, 0>(*stress_energy) = square(get(lapse) * get(pi_scalar));
+  const double kappa = 8.0 * M_PI;
+  get<0, 0>(*stress_energy) = kappa * square(get(lapse) * get(pi_scalar));
   for (size_t i = 0; i < 3; ++i) {
     stress_energy->get(0, i + 1) =
-        -get(lapse) * get(pi_scalar) * phi_scalar.get(i);
+        -kappa * get(lapse) * get(pi_scalar) * phi_scalar.get(i);
   }
   for (size_t i = 0; i < 3; ++i) {
     for (size_t j = i; j < 3; ++j) {
-      stress_energy->get(i + 1, j + 1) = phi_scalar.get(i) * phi_scalar.get(j);
+      stress_energy->get(i + 1, j + 1) =
+          kappa * phi_scalar.get(i) * phi_scalar.get(j);
     }
   }
 }
