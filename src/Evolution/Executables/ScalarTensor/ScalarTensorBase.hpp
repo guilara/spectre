64558e7eeb97b61d959146efde0b3e3cd564c2a8
Guillermo Lara <glara@aei.mpg.de>
2024-05-04 16:19:27 +0000

    Add tag to exec

--- src/Evolution/Executables/ScalarTensor/ScalarTensorBase.hpp
+++ src/Evolution/Executables/ScalarTensor/ScalarTensorBase.hpp
@@ -271,6 +271,8 @@ struct ObserverTags {
           ScalarTensor::Tags::GBScalarCompute<DataVector>,
           ScalarTensor::Tags::CouplingFunctionDerivativeCompute<DataVector>,
           // BR
+          ScalarTensor::Tags::WeylElectricFullCompute<DataVector, 3,
+                                                      Frame::Inertial>,
           ScalarTensor::Tags::WeylElectricRicciCompute<DataVector, 3,
                                                        Frame::Inertial>,
           ScalarTensor::Tags::WeylElectricRicciScalarComplementCompute<
