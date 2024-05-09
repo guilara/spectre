3f33059fd2944eacdb0c5f59263a3856dba45d17
Guillermo Lara <glara@aei.mpg.de>
2024-05-09 11:06:55 +0000

    Add compute tags

--- src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/Sources.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/Sources.hpp
@@ -43,12 +43,43 @@ namespace fe::ScalarTensorDriver::Tags {
 /*!
  * \brief Compute tag for the tensor driver source.
  */
-struct TensorDriverSourceCompute {};
+struct TensorDriverSourceCompute
+    : TensorDriverSource<DataVector, 3, Frame::Inertial>,
+      db::ComputeTag {
+  using argument_tags =
+      tmpl::list<fe::ScalarTensorDriver::Tags::TensorDriver<DataVector, 3,
+                                                            Frame::Inertial>,
+                 fe::ScalarTensorDriver::Tags::TargetTensor<DataVector, 3,
+                                                            Frame::Inertial>,
+                 fe::ScalarTensorDriver::Tags::TauParameter,
+                 fe::ScalarTensorDriver::Tags::SigmaParameter>;
+  using return_type = tnsr::aa<DataVector, 3, Frame::Inertial>;
+  static constexpr void (*function)(
+      gsl::not_null<tnsr::aa<DataVector, 3>*> tensor_driver_source,
+      const tnsr::aa<DataVector>& tensor_driver,
+      const tnsr::aa<DataVector, 3>& target_tensor,
+      const Scalar<DataVector>& scalar_tau_parameter,
+      const Scalar<DataVector>& scalar_sigma_parameter) =
+      &fe::ScalarTensorDriver::Sources::compute_tensor_driver_source;
+  using base = TensorDriverSource<DataVector, 3, Frame::Inertial>;
+};
 
 /*!
  * \brief Compute tag for the tensor driver target.
+ * \details TODO : Replace with backreaction trace-reversed H tensor
  */
 template <typename Frame, typename DataType>
-struct TargetTensorCompute {};
+struct TargetTensorCompute : TargetTensor<DataVector, 3, Frame::Inertial>,
+                             db::ComputeTag {
+  using argument_tags =
+      tmpl::list<fe::ScalarTensorDriver::Tags::TensorDriver<DataVector, 3,
+                                                            Frame::Inertial>>;
+  using return_type = tnsr::aa<DataVector, 3, Frame::Inertial>;
+  static constexpr void (*function)(
+      gsl::not_null<tnsr::aa<DataVector, a>*> target_tensor,
+      const tnsr::aa<DataVector, 3>& tensor_driver) =
+      &fe::ScalarTensorDriver::Sources::compute_target_tensor;
+  using base = TargetTensor<DataVector, 3, Frame::Inertial>;
+};
 
 }  // namespace fe::ScalarTensorDriver::Tags
