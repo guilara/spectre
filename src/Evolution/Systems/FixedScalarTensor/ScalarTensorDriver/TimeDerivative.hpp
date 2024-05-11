e054374618276d71c3a91f0cfbc73656cfcc1ee3
Guillermo Lara <glara@aei.mpg.de>
2024-05-11 09:38:07 +0000

    Fix tags and arguments

--- src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/TimeDerivative.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/TimeDerivative.hpp
@@ -36,30 +36,29 @@ struct TimeDerivative {
 
   using temporary_tags = tmpl::list<
       // Tensor driver temporary tags
-      gr::Tags::Lapse<DataVector>, gr::Tags::Shift<DataVector, Dim>,
-      gr::Tags::InverseSpatialMetric<DataVector, Dim>,
-      gr::Tags::DetSpatialMetric<DataVector>,
+      gr::Tags::Lapse<DataVector>, gr::Tags::Shift<DataVector, dim>,
+      gr::Tags::InverseSpatialMetric<DataVector, dim>,
+      gr::Tags::DetSpatialMetric<DataVector>
       // Extra scalar driver temporary tags
-      fe::ScalarTensorDriver::Tags::TensorDriverSource<DataVector, dim,
-                                                       ::Frame::Inertial>>;
+      >;
 
   using argument_tags = tmpl::list<
       // Tensor argument tags
-      fe::ScalarTensorDriver::Tags::TensorDriver<DataVector, Dim>,
-      fe::ScalarTensorDriver::Tags::Pi<DataVector, Dim>,
-      fe::ScalarTensorDriver::Tags::Phi<DataVector, Dim>,
+      fe::ScalarTensorDriver::Tags::TensorDriver<DataVector, dim>,
+      fe::ScalarTensorDriver::Tags::Pi<DataVector, dim>,
 
-      domain::Tags::Mesh<Dim>, ::Tags::Time,
-      domain::Tags::Coordinates<Dim, Frame::Inertial>,
-      domain::Tags::InverseJacobian<Dim, Frame::ElementLogical,
+      domain::Tags::Mesh<dim>, ::Tags::Time,
+      domain::Tags::Coordinates<dim, Frame::Inertial>,
+      domain::Tags::InverseJacobian<dim, Frame::ElementLogical,
                                     Frame::Inertial>,
-      domain::Tags::MeshVelocity<Dim, Frame::Inertial>,
+      domain::Tags::MeshVelocity<dim, Frame::Inertial>,
       // Scalar argument tags
-      fe::ScalarTensorDriver::Tags::PiScalar,
-      fe::ScalarTensorDriver::Tags::PhiScalar<Dim>, gr::Tags::Lapse<DataVector>,
-      gr::Tags::Shift<DataVector, Dim>,
+      fe::ScalarTensorDriver::Tags::Psi, fe::ScalarTensorDriver::Tags::PiScalar,
+      gr::Tags::Lapse<DataVector>, gr::Tags::Shift<DataVector, dim>,
 
       // Extra argument tags
+      fe::ScalarTensorDriver::Tags::TensorDriverSource<DataVector, dim,
+                                                       ::Frame::Inertial>,
       fe::ScalarTensorDriver::Tags::ScalarDriverSource,
       // TODO: Add sigma and tau parameters
       fe::ScalarTensorDriver::Tags::TauParameter,
@@ -85,7 +84,7 @@ struct TimeDerivative {
       // Scalar temporal variables
 
       // Extra temporal tags
-      gsl::not_null<tnsr::aa<DataVector, dim>*> stress_energy,
+      //   gsl::not_null<tnsr::aa<DataVector, dim>*> stress_energy,
 
       // GH spatial derivatives
       const tnsr::iaa<DataVector, dim>& d_spacetime_metric,
@@ -100,8 +99,6 @@ struct TimeDerivative {
       // GH argument variables
       const tnsr::aa<DataVector, dim>& spacetime_metric,
       const tnsr::aa<DataVector, dim>& pi,
-      //   const tnsr::iaa<DataVector, dim>& phi,
-      const Scalar<DataVector>& gamma0,
 
       const Mesh<dim>& mesh, double time,
       const tnsr::I<DataVector, dim, Frame::Inertial>& inertial_coords,
@@ -111,11 +108,12 @@ struct TimeDerivative {
           mesh_velocity,
 
       // Scalar argument variables
-      const Scalar<DataVector>& pi_scalar,
+      const Scalar<DataVector>& psi, const Scalar<DataVector>& pi_scalar,
       //   const tnsr::i<DataVector, dim>& phi_scalar,
       const Scalar<DataVector>& lapse_scalar,
       const tnsr::I<DataVector, dim>& shift_scalar,
 
+      const tnsr::aa<DataVector, dim>& tensor_driver_source,
       const Scalar<DataVector>& scalar_source,
 
       const Scalar<DataVector>& tau_parameter,
