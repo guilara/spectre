8536d20c80dac4c62d6d9e1e84b1e4a743474293
Guillermo Lara <glara@aei.mpg.de>
2024-05-11 16:01:32 +0000

    Add wrapped gr

--- src/PointwiseFunctions/AnalyticData/GhFixedScalarTensor/InstantiateWrappedGr.cpp
+++ src/PointwiseFunctions/AnalyticData/GhFixedScalarTensor/InstantiateWrappedGr.cpp
@@ -5,6 +5,7 @@
 #include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/KerrSphericalHarmonic.hpp"
 #include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/MinkowskiZeroScalar.hpp"
 #include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedDecoupledScalar/SphericalKerrSchildSH.hpp"
+#include "PointwiseFunctions/AnalyticData/FixedScalarTensor/FixedSGB/KerrSphericalHarmonic.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/WrappedGr.tpp"
 #include "Utilities/GenerateInstantiations.hpp"
@@ -13,4 +14,5 @@ GENERATE_INSTANTIATIONS(
     WRAPPED_GR_INSTANTIATE,
     (fe::DecoupledScalar::AnalyticData::MinkowskiZeroScalar,
      fe::DecoupledScalar::AnalyticData::KerrSphericalHarmonic,
-     fe::DecoupledScalar::AnalyticData::SphericalKerrSchildSH))
+     fe::DecoupledScalar::AnalyticData::SphericalKerrSchildSH,
+     fe::sgb::AnalyticData::KerrSphericalHarmonic))
