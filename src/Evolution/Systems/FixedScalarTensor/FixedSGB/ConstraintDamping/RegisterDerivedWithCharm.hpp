d3f01669de7c4722d41a11d8f8a461c779eb077f
Guillermo Lara <glara@aei.mpg.de>
2024-05-11 15:23:33 +0000

    Add constraint damping

---
+++ src/Evolution/Systems/FixedScalarTensor/FixedSGB/ConstraintDamping/RegisterDerivedWithCharm.hpp
@@ -0,0 +1,8 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace fe::sgb::ConstraintDamping {
+void register_derived_with_charm();
+}  // namespace fe::sgb::ConstraintDamping
