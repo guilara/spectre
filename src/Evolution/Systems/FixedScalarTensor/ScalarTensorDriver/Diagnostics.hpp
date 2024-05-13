7ebeac522550598a3a088b2dc7530767941ba3f6
Guillermo Lara <glara@aei.mpg.de>
2024-05-13 13:19:17 +0000

    Various fixes

--- src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/Diagnostics.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/Diagnostics.hpp
@@ -50,24 +50,24 @@ struct ScalarTrackingDiagnosticCompute : ScalarTrackingDiagnostic,
  *
  * \details Compute the difference between the tensor driver and its target.
  */
-struct TensorTrackingDiagnosticCompute : TensorTrackingDiagnostic,
-                                         db::ComputeTag {
+template <typename Frame, typename DataType>
+struct TensorTrackingDiagnosticCompute
+    : TensorTrackingDiagnostic<DataType, 3, Frame>,
+      db::ComputeTag {
   using argument_tags =
-      tmpl::list<fe::ScalarTensorDriver::Tags::TensorDriver<DataVector, 3,
-                                                            Frame::Inertial>,
-                 fe::ScalarTensorDriver::Tags::TargetTensor<DataVector, 3,
-                                                            Frame::Inertial>,
+      tmpl::list<fe::ScalarTensorDriver::Tags::TensorDriver<DataType, 3, Frame>,
+                 fe::ScalarTensorDriver::Tags::TargetTensor<DataType, 3, Frame>,
                  fe::ScalarTensorDriver::Tags::TauParameter,
                  fe::ScalarTensorDriver::Tags::SigmaParameter>;
-  using return_type = tnsr::aa<DataVector, 3, Frame::Inertial>;
+  using return_type = tnsr::aa<DataType, 3, Frame>;
   static constexpr void (*function)(
-      const gsl::not_null<tnsr::aa<DataVector, 3>*> diagnostic,
-      const tnsr::aa<DataVector, 3>& tensor_driver,
-      const tnsr::aa<DataVector, 3>& target_tensor,
-      const Scalar<DataVector>& scalar_tau_parameter,
-      const Scalar<DataVector>& scalar_sigma_parameter) =
+      const gsl::not_null<tnsr::aa<DataType, 3, Frame>*> diagnostic,
+      const tnsr::aa<DataType, 3, Frame>& tensor_driver,
+      const tnsr::aa<DataType, 3, Frame>& target_tensor,
+      const Scalar<DataType>& scalar_tau_parameter,
+      const Scalar<DataType>& scalar_sigma_parameter) =
       &tensor_driver_tracking_diagnostic;
-  using base = TensorTrackingDiagnostic;
+  using base = TensorTrackingDiagnostic<DataType, 3, Frame>;
 };
 
 }  // namespace Tags
