53073b682a38f96545de78f2336f2ad6c26ecdad
Guillermo Lara <glara@aei.mpg.de>
2024-05-29 15:26:37 +0000

    Comment tags

--- src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/Sources.hpp
+++ src/Evolution/Systems/FixedScalarTensor/ScalarTensorDriver/Sources.hpp
@@ -156,8 +156,10 @@ struct TensorDriverSourceCompute : TensorDriverSource<DataType, 3, Frame>,
  */
 template <typename Frame, typename DataType>
 struct TargetTensorCompute : TargetTensor<DataType, 3, Frame>, db::ComputeTag {
-  using argument_tags =
-      tmpl::list<ScalarTensor::Tags::OrderReducedTraceReversedStressEnergy>;
+  //   using argument_tags =
+  //      tmpl::list<ScalarTensor::Tags::OrderReducedTraceReversedStressEnergy>;
+  using argument_tags = tmpl::list<
+      ScalarTensor::Tags::TraceReversedStressEnergy<DataType, 3, Frame>>;
   using return_type = tnsr::aa<DataType, 3, Frame>;
   static constexpr void (*function)(
       gsl::not_null<tnsr::aa<DataType, 3, Frame>*> target_tensor,
