9c056bcf12c57f6fecf0c96b1dd39149ac850d77
Guillermo Lara <glara@aei.mpg.de>
2024-06-10 15:42:50 +0000

    Fix missing coupling in gbscalar

--- src/PointwiseFunctions/ScalarTensor/GBScalarSourceTerms.cpp
+++ src/PointwiseFunctions/ScalarTensor/GBScalarSourceTerms.cpp
@@ -37,18 +37,63 @@ void order_reduced_gb_scalar_with_tenex(
                             inverse_spacetime_metric(ti::D, ti::C));
 }
 
+template <typename Frame>
+void order_reduced_gb_scalar_with_tenex(
+    const gsl::not_null<Scalar<DataVector>*> result,
+    const Scalar<DataVector>& weyl_electric_scalar,
+    const Scalar<DataVector>& weyl_magnetic_scalar,
+    const tnsr::aa<DataVector, 3, Frame>& trace_reversed_stress_energy,
+    const Scalar<DataVector>& trace_of_trace_reversed_stress_energy,
+    const tnsr::AA<DataVector, 3, Frame>& inverse_spacetime_metric,
+    const tnsr::aa<DataVector, 3, Frame>& tensor_driver,
+    const Scalar<DataVector>& trace_of_tensor_driver) {
+  static constexpr double two_over_three = 2.0 / 3.0;
+  tenex::evaluate(
+      result,
+      // Weyl squared in terms of electric and magnetic scalars
+      // with complement
+      8.0 * (weyl_electric_scalar() - weyl_magnetic_scalar())
+          // Trace reversed stress energy squared
+          - 2.0 * trace_reversed_stress_energy(ti::a, ti::b) *
+                inverse_spacetime_metric(ti::B, ti::C) *
+                trace_reversed_stress_energy(ti::c, ti::d) *
+                inverse_spacetime_metric(ti::D, ti::A)
+          // Square of the trace of the trace reversed stress energy
+          + two_over_three * trace_of_trace_reversed_stress_energy() *
+                trace_of_trace_reversed_stress_energy()
+
+          // Tensor driver terms
+          - 2.0 * tensor_driver(ti::a, ti::b) *
+                inverse_spacetime_metric(ti::B, ti::C) *
+                tensor_driver(ti::c, ti::d) *
+                inverse_spacetime_metric(ti::D, ti::A)
+          // Square of the trace of the tensor driver
+          +
+          two_over_three * trace_of_tensor_driver() * trace_of_tensor_driver());
+}
+
 }  // namespace ScalarTensor
 
 #define FRAME(data) BOOST_PP_TUPLE_ELEM(0, data)
 
-#define INSTANTIATE(_, data)                                      \
-  template void ScalarTensor::order_reduced_gb_scalar_with_tenex( \
-      const gsl::not_null<Scalar<DataVector>*> result,            \
-      const Scalar<DataVector>& weyl_electric_scalar,             \
-      const Scalar<DataVector>& weyl_magnetic_scalar,             \
-      const tnsr::aa<DataVector, 3, FRAME(data)>&                 \
-          trace_reversed_stress_energy,                           \
-      const tnsr::AA<DataVector, 3, FRAME(data)>& inverse_spacetime_metric);
+#define INSTANTIATE(_, data)                                                 \
+  template void ScalarTensor::order_reduced_gb_scalar_with_tenex(            \
+      const gsl::not_null<Scalar<DataVector>*> result,                       \
+      const Scalar<DataVector>& weyl_electric_scalar,                        \
+      const Scalar<DataVector>& weyl_magnetic_scalar,                        \
+      const tnsr::aa<DataVector, 3, FRAME(data)>&                            \
+          trace_reversed_stress_energy,                                      \
+      const tnsr::AA<DataVector, 3, FRAME(data)>& inverse_spacetime_metric); \
+  template void ScalarTensor::order_reduced_gb_scalar_with_tenex(            \
+      const gsl::not_null<Scalar<DataVector>*> result,                       \
+      const Scalar<DataVector>& weyl_electric_scalar,                        \
+      const Scalar<DataVector>& weyl_magnetic_scalar,                        \
+      const tnsr::aa<DataVector, 3, FRAME(data)>&                            \
+          trace_reversed_stress_energy,                                      \
+      const Scalar<DataVector>& trace_of_trace_reversed_stress_energy,       \
+      const tnsr::AA<DataVector, 3, FRAME(data)>& inverse_spacetime_metric,  \
+      const tnsr::aa<DataVector, 3, FRAME(data)>& tensor_driver,             \
+      const Scalar<DataVector>& trace_of_tensor_driver);
 
 GENERATE_INSTANTIATIONS(INSTANTIATE, (Frame::Grid, Frame::Inertial))
 
