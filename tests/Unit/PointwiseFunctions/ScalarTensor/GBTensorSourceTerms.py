59b706099c326a94dfc88b6fe02192c758c8611a
Guillermo Lara <glara@aei.mpg.de>
2024-06-21 09:46:03 +0000

    Fix test

--- tests/Unit/PointwiseFunctions/ScalarTensor/GBTensorSourceTerms.py
+++ tests/Unit/PointwiseFunctions/ScalarTensor/GBTensorSourceTerms.py
@@ -4,9 +4,9 @@
 import numpy as np
 
 
-def spacetime_derivative_scalar(lapse, pi_scalar, phi_scalar):
-    # n_{0} Pi; where n_{0} = - lapse
-    deriv_0 = -lapse * pi_scalar
+def spacetime_derivative_scalar(lapse, shift, pi_scalar, phi_scalar):
+    # n_{0} Pi + gamma^{i}_{0} Phi_{i}; where n_{0} = - lapse
+    deriv_0 = -lapse * pi_scalar + np.dot(shift, phi_scalar)
 
     spacetime_derivative_scalar = np.concatenate(([deriv_0], phi_scalar))
 
