69ead0dff20df2fae482e02615d8693ac6f1bad7
Guillermo Lara <glara@aei.mpg.de>
2024-06-23 16:30:23 +0000

    Add test of canonical SET

--- tests/Unit/PointwiseFunctions/ScalarTensor/GBTensorSourceTerms.py
+++ tests/Unit/PointwiseFunctions/ScalarTensor/GBTensorSourceTerms.py
@@ -378,3 +378,30 @@ def tensor_driver_spatial_spatial_projection(
     result = tensor_driver[1:, 1:]
 
     return result
+
+
+# This test should be moved to Test_ScalarTensor
+def trace_reversed_stress_energy(pi_scalar, phi_scalar, lapse, shift):
+    # Define the different components
+    rho = pi_scalar * pi_scalar
+    j_vec = -pi_scalar * phi_scalar
+    S = np.outer(phi_scalar, phi_scalar)
+
+    # Construct the trace-reversed stress energy tensor
+    stress_energy = np.zeros((4, 4))
+
+    # 00-component
+    stress_energy[0, 0] = (
+        np.power(lapse, 2) * rho
+        + 2.0 * lapse * np.dot(shift, j_vec)
+        + shift @ S @ shift
+    )
+
+    # 0i-component
+    stress_energy[0, 1:] = lapse * j_vec + shift @ S
+    stress_energy[1:, 0] = np.transpose(stress_energy[0, 1:])
+
+    # ij-component
+    stress_energy[1:, 1:] = S
+
+    return stress_energy
