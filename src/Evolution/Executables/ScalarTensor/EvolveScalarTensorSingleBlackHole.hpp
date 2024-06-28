9c4bfad649b3f68db97a2a99d6f60834fb5e7cde
Guillermo Lara <glara@aei.mpg.de>
2024-06-28 16:31:20 +0000

    Add bondi sachs for single bh

--- src/Evolution/Executables/ScalarTensor/EvolveScalarTensorSingleBlackHole.hpp
+++ src/Evolution/Executables/ScalarTensor/EvolveScalarTensorSingleBlackHole.hpp
@@ -180,15 +180,34 @@ struct EvolutionMetavars : public ScalarTensorTemplateBase<EvolutionMetavars> {
   static constexpr bool use_control_systems =
       tmpl::size<control_systems>::value > 0;
 
+  struct BondiSachs;
+
   using interpolation_target_tags = tmpl::push_back<
       control_system::metafunctions::interpolation_target_tags<control_systems>,
       AhA, ExcisionBoundaryA, SphericalSurface, SphericalSurface2,
       SphericalSurface3, SphericalSurface4, SphericalSurface5,
-      SphericalSurface6>;
+      SphericalSurface6, BondiSachs>;
   using interpolator_source_vars = ::ah::source_vars<volume_dim>;
+  using source_vars_no_deriv =
+      tmpl::list<gr::Tags::SpacetimeMetric<DataVector, volume_dim>,
+                 gh::Tags::Pi<DataVector, volume_dim>,
+                 gh::Tags::Phi<DataVector, volume_dim>>;
 
   using scalar_charge_interpolator_source_vars =
       detail::ObserverTags::scalar_charge_vars_to_interpolate_to_target;
+
+  struct BondiSachs : tt::ConformsTo<intrp::protocols::InterpolationTargetTag> {
+    static std::string name() { return "BondiSachsInterpolation"; }
+    using temporal_id = ::Tags::Time;
+    using vars_to_interpolate_to_target = source_vars_no_deriv;
+    using compute_target_points =
+        intrp::TargetPoints::Sphere<BondiSachs, ::Frame::Inertial>;
+    using post_interpolation_callbacks =
+        tmpl::list<intrp::callbacks::DumpBondiSachsOnWorldtube<BondiSachs>>;
+    using compute_items_on_target = tmpl::list<>;
+    template <typename Metavariables>
+    using interpolating_component = typename Metavariables::st_dg_element_array;
+  };
 
   // The interpolator_source_vars need to be the same in both the
   // Interpolate event and the InterpolateWithoutInterpComponent event.  The
@@ -213,6 +232,8 @@ struct EvolutionMetavars : public ScalarTensorTemplateBase<EvolutionMetavars> {
                                            interpolator_source_vars>,
                 control_system::metafunctions::control_system_events<
                     control_systems>,
+                intrp::Events::InterpolateWithoutInterpComponent<
+                    3, BondiSachs, source_vars_no_deriv>,
                 intrp::Events::InterpolateWithoutInterpComponent<
                     volume_dim, ExcisionBoundaryA, interpolator_source_vars>,
                 intrp::Events::InterpolateWithoutInterpComponent<
