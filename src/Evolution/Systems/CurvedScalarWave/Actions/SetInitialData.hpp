55556a81719f4ba9d275870ef418768ac6a5e376
Guillermo Lara <glara@aei.mpg.de>
2024-07-15 16:32:29 +0000

    Clean

--- src/Evolution/Systems/CurvedScalarWave/Actions/SetInitialData.hpp
+++ src/Evolution/Systems/CurvedScalarWave/Actions/SetInitialData.hpp
@@ -35,45 +35,25 @@ namespace CurvedScalarWave {
 
 /*!
  * \brief Numeric initial data loaded from volume data files
- *
- * This class can be factory-created in the input file to start an evolution
- * from numeric initial data. It selects the hydro variables to load from the
- * volume data files and allows to choose constant values for some of them.
- *
- * Where the density is below the `DensityCutoff` the fluid variables are set to
- * vacuum (zero density, pressure, energy and velocity, and unit Lorentz
- * factor). To evolve the initial data, an atmosphere treatment is likely
- * required to fix the value of the fluid variables in these regions.
  */
 class NumericInitialData : public evolution::initial_data::InitialData {
  public:
-  /// Name of a variable in the volume data file. Can be optional, in which case
-  /// a constant value can be supplied instead of a dataset name.
-  template <typename Tag, typename IsRequired>
+  template <typename Tag>
   struct VarName {
     using tag = Tag;
-    static constexpr bool is_required = IsRequired::value;
     static std::string name() { return db::tag_name<Tag>(); }
-    using type = std::conditional_t<is_required, std::string,
-                                    std::variant<double, std::string>>;
+    using type = std::string;
     static constexpr Options::String help =
-        "Name of the variable in the volume data file. For optional variables "
-        "you may instead specify a double that is used as a constant value "
-        "on the entire grid.";
+        "Name of the variable in the volume data file";
   };
 
-  // These are the hydro variables that we support loading from volume
+  // These are the scalar variables that we support loading from volume
   // data files
-  using required_primitive_vars =
+  using all_vars =
       tmpl::list<CurvedScalarWave::Tags::Psi, CurvedScalarWave::Tags::Pi>;
   using optional_primitive_vars = tmpl::list<>;
-  using primitive_vars_option_tags =
-      tmpl::append<db::wrap_tags_in<VarName, required_primitive_vars,
-                                    std::bool_constant<true>>,
-                   db::wrap_tags_in<VarName, optional_primitive_vars,
-                                    std::bool_constant<false>>>;
-  struct PrimitiveVars
-      : tuples::tagged_tuple_from_typelist<primitive_vars_option_tags> {
+
+  struct ScalarVars : tuples::tagged_tuple_from_typelist<all_vars> {
     static constexpr Options::String help =
         "Primitive hydro variables: 'RestMassDensity' and "
         "'LowerSpatialFourVelocity' (which is u_i = W * gamma_ij v^j). ";
@@ -81,28 +61,12 @@ class NumericInitialData : public evolution::initial_data::InitialData {
     using TaggedTuple::TaggedTuple;
   };
 
-  using all_vars =
-      tmpl::append<required_primitive_vars, optional_primitive_vars>;
-
   // Input-file options
   struct Variables {
-    using type = PrimitiveVars;
+    using type = ScalarVars;
     static constexpr Options::String help =
         "Set of initial data variables from which the Valencia evolution "
         "variables are computed.";
-  };
-
-  struct DensityCutoff {
-    using type = double;
-    static constexpr Options::String help =
-        "Where the density is below this cutoff the fluid variables are set to "
-        "vacuum (zero density, pressure, energy and velocity, and unit Lorentz "
-        "factor). "
-        "During the evolution, atmosphere treatment will typically kick in and "
-        "fix the value of the fluid variables in these regions. Therefore, "
-        "it makes sense to set this density cutoff to the same value as the "
-        "atmosphere density cutoff.";
-    static constexpr double lower_bound() { return 0.; }
   };
 
   using options =
@@ -137,47 +101,37 @@ class NumericInitialData : public evolution::initial_data::InitialData {
   NumericInitialData(
       std::string file_glob, std::string subfile_name,
       std::variant<double, importers::ObservationSelector> observation_value,
-      bool enable_interpolation, PrimitiveVars selected_variables
-      //   ,
-      //   double density_cutoff
-  );
+      bool enable_interpolation, ScalarVars selected_variables);
 
   const importers::ImporterOptions& importer_options() const {
     return importer_options_;
   }
 
-  const PrimitiveVars& selected_variables() const {
-    return selected_variables_;
-  }
-
-  //   double density_cutoff() const { return density_cutoff_; }
+  const ScalarVars& selected_variables() const { return selected_variables_; }
 
   size_t volume_data_id() const;
 
   template <typename... AllTags>
   void select_for_import(
-      const gsl::not_null<tuples::TaggedTuple<AllTags...>*> all_fields) const {
+      const gsl::not_null<tuples::TaggedTuple<AllTags...>*> fields) const {
     // Select the subset of the available variables that we want to read from
     // the volume data file
-    tmpl::for_each<primitive_vars_option_tags>([&all_fields,
-                                                this](const auto option_tag_v) {
-      using option_tag = tmpl::type_from<std::decay_t<decltype(option_tag_v)>>;
-      using tag = typename option_tag::tag;
-      static constexpr bool is_required = option_tag::is_required;
-      const auto& selected_dataset_name = get<option_tag>(selected_variables_);
-      if constexpr (is_required) {
-        // Always select required tags for import
-        get<importers::Tags::Selected<tag>>(*all_fields) =
-            selected_dataset_name;
-      } else {
-        // Only select optional tags for import if a dataset name was
-        // specified
-        if (std::holds_alternative<std::string>(selected_dataset_name)) {
-          get<importers::Tags::Selected<tag>>(*all_fields) =
-              std::get<std::string>(selected_dataset_name);
-        }
-      }
-    });
+    std::visit(
+        [&fields](const auto& vars) {
+          // This lambda is invoked with the set of vars selected in the input
+          // file, which map to the tensor names that should be read from the H5
+          // file
+          using selected_vars = std::decay_t<decltype(vars)>;
+          // Get the mapped tensor name from the input file and select it in the
+          // set of all possible vars.
+          tmpl::for_each<typename selected_vars::tags_list>(
+              [&fields, &vars](const auto tag_v) {
+                using tag = typename std::decay_t<decltype(tag_v)>::type::tag;
+                get<importers::Tags::Selected<tag>>(*fields) =
+                    get<VarName<tag>>(vars);
+              });
+        },
+        selected_variables_);
   }
 
   template <typename... AllTags>
@@ -202,8 +156,7 @@ class NumericInitialData : public evolution::initial_data::InitialData {
 
  private:
   importers::ImporterOptions importer_options_{};
-  PrimitiveVars selected_variables_{};
-//   double density_cutoff_{};
+  ScalarVars selected_variables_{};
 };
 
 namespace Actions {
