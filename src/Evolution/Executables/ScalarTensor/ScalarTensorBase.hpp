ba5984ac7b785dc681bde3384462266d186f3d18
Guillermo Lara <glara@aei.mpg.de>
2024-07-18 11:21:26 +0000

    Swap numerical id and and pert

--- src/Evolution/Executables/ScalarTensor/ScalarTensorBase.hpp
+++ src/Evolution/Executables/ScalarTensor/ScalarTensorBase.hpp
@@ -189,6 +189,8 @@ struct ScalarTensorTemplateBase;
 namespace detail {
 constexpr auto make_default_phase_order() {
   return std::array{Parallel::Phase::Initialization,
+                    Parallel::Phase::RegisterWithElementDataReader,
+                    Parallel::Phase::ImportInitialData,
                     Parallel::Phase::InitializeInitialDataDependentQuantities,
                     Parallel::Phase::Register,
                     Parallel::Phase::InitializeTimeStepperHistory,
@@ -390,7 +392,10 @@ struct FactoryCreation : tt::ConformsTo<Options::protocols::FactoryCreation> {
           ScalarTensor::BoundaryConditions::BoundaryCondition,
           ScalarTensor::BoundaryConditions::standard_boundary_conditions>,
       tmpl::pair<gh::gauges::GaugeCondition, gh::gauges::all_gauges>,
-      tmpl::pair<evolution::initial_data::InitialData, initial_data_list>,
+      tmpl::pair<evolution::initial_data::InitialData,
+                 //   tmpl::push_back<initial_data_list,
+                 //   ScalarTensor::NumericInitialData>
+                 tmpl::list<ScalarTensor::NumericInitialData>>,
       tmpl::pair<LtsTimeStepper, TimeSteppers::lts_time_steppers>,
       tmpl::pair<PhaseChange, PhaseControl::factory_creatable_classes>,
       tmpl::pair<StepChooser<StepChooserUse::LtsStep>,
@@ -506,8 +511,8 @@ struct ScalarTensorTemplateBase {
           ::amr::Initialization::Initialize<volume_dim>,
           Initialization::TimeStepperHistory<derived_metavars>>,
       Initialization::Actions::NonconservativeSystem<system>,
-      evolution::Initialization::Actions::SetVariables<
-          domain::Tags::Coordinates<volume_dim, Frame::ElementLogical>>,
+      //   evolution::Initialization::Actions::SetVariables<
+      //       domain::Tags::Coordinates<volume_dim, Frame::ElementLogical>>,
       // Random noise system::variables_tag
       //   Actions::RandomizeVariables<typename system::variables_tag,
       //                               RandomizeInitialGuess>,
