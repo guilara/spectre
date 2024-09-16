36b7d96c8bef0b68a7032e718b43057827fc2cb0
guilara <guillermo.lara.delgado@gmail.com>
2024-09-16 17:07:32 +0200

    Increase gamma0 at center

--- support/Pipelines/Bbh/Inspiral.py
+++ support/Pipelines/Bbh/Inspiral.py
@@ -63,13 +63,14 @@ def _constraint_damping_params(
     initial_separation: float,
 ) -> dict:
     total_mass = mass_left + mass_right
+    # Change central gaussian amplitude by 10x
     return {
         "Gamma0Constant": 0.001 / total_mass,
         "Gamma0LeftAmplitude": 4.0 / mass_left,
         "Gamma0LeftWidth": 7.0 * mass_left,
         "Gamma0RightAmplitude": 4.0 / mass_right,
         "Gamma0RightWidth": 7.0 * mass_right,
-        "Gamma0OriginAmplitude": 0.075 / total_mass,
+        "Gamma0OriginAmplitude": 0.75 / total_mass,
         "Gamma0OriginWidth": 2.5 * initial_separation,
         "Gamma1Width": 10.0 * initial_separation,
     }
